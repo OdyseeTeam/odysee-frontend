dd052c70b431a1f6937bc8b836349b14e8e12f59
GG2015 <inquiry.globalgamer2015@gmail.com>
2021-10-07 06:00:36 -0400

    Fixes image when previewing markdown.

--- ui/component/common/markdown-preview.jsx
+++ ui/component/common/markdown-preview.jsx
@@ -8,41 +8,19 @@ import remarkEmoji from 'remark-emoji';
 import remarkBreaks from 'remark-breaks';
 import remarkFrontMatter from 'remark-frontmatter';
 import reactRenderer from 'remark-react';
-import MarkdownLink from 'component/markdownLink';
 import defaultSchema from 'hast-util-sanitize/lib/github.json';
 import { formatedLinks, inlineLinks } from 'util/remark-lbry';
 import { formattedTimestamp, inlineTimestamp } from 'util/remark-timestamp';
-import ZoomableImage from 'component/zoomableImage';
-import { CHANNEL_STAKED_LEVEL_VIDEO_COMMENTS, SIMPLE_SITE } from 'config';
-import Button from 'component/button';
-import * as ICONS from 'constants/icons';
 
 type SimpleTextProps = {
   children?: React.Node,
 };
 
-type SimpleLinkProps = {
-  href?: string,
-  title?: string,
-  children?: React.Node,
-};
-
-type ImageLinkProps = {
-  src: string,
-  title?: string,
-  alt?: string,
-  helpText?: string,
-};
-
 type MarkdownProps = {
   strip?: boolean,
   content: ?string,
-  simpleLinks?: boolean,
-  noDataStore?: boolean,
   className?: string,
-  parentCommentId?: string,
   isMarkdownPost?: boolean,
-  stakedLevel?: number,
 };
 
 // ****************************************************************************
@@ -50,63 +28,6 @@ type MarkdownProps = {
 
 const SimpleText = (props: SimpleTextProps) => {
   return <span>{props.children}</span>;
-};
-
-// ****************************************************************************
-// ****************************************************************************
-
-const SimpleLink = (props: SimpleLinkProps) => {
-  const { title, children, href } = props;
-
-  if (!href) {
-    return children || null;
-  }
-
-  if (!href.startsWith('lbry:/')) {
-    return (
-      <a href={href} title={title} target={'_blank'} rel={'noreferrer noopener'}>
-        {children}
-      </a>
-    );
-  }
-
-  const [uri, search] = href.split('?');
-  const urlParams = new URLSearchParams(search);
-  const embed = urlParams.get('embed');
-
-  if (embed) {
-    // Decode this since users might just copy it from the url bar
-    const decodedUri = decodeURI(uri);
-    return (
-      <div className="embed__inline-button-preview">
-        <pre>{decodedUri}</pre>
-      </div>
-    );
-  }
-
-  // Dummy link (no 'href')
-  return <a title={title}>{children}</a>;
-};
-
-// ****************************************************************************
-// ****************************************************************************
-
-const SimpleImageLink = (props: ImageLinkProps) => {
-  const { src, title, alt, helpText } = props;
-  if (!src) {
-    return null;
-  }
-
-  return (
-    <Button
-      button="link"
-      iconRight={ICONS.EXTERNAL}
-      label={title || alt || src}
-      title={helpText || title || alt || src}
-      className="button--external-link"
-      href={src}
-    />
-  );
 };
 
 // ****************************************************************************
@@ -124,15 +45,8 @@ const REPLACE_REGEX = /(<iframe\s+src=["'])(.*?(?=))(["']\s*><\/iframe>)/g;
 // ****************************************************************************
 // ****************************************************************************
 
-function isStakeEnoughForPreview(stakedLevel) {
-  return !stakedLevel || stakedLevel >= CHANNEL_STAKED_LEVEL_VIDEO_COMMENTS;
-}
-
-// ****************************************************************************
-// ****************************************************************************
-
 const MarkdownPreview = (props: MarkdownProps) => {
-  const { content, strip, simpleLinks, noDataStore, className, parentCommentId, isMarkdownPost, stakedLevel } = props;
+  const { content, strip, className, isMarkdownPost } = props;
   const strippedContent = content
     ? content.replace(REPLACE_REGEX, (iframeHtml, y, iframeSrc) => {
         // Let the browser try to create an iframe to see if the markup is valid
@@ -155,33 +69,7 @@ const MarkdownPreview = (props: MarkdownProps) => {
   const remarkOptions: Object = {
     sanitize: schema,
     fragment: React.Fragment,
-    remarkReactComponents: {
-      a: noDataStore
-        ? SimpleLink
-        : (linkProps) => (
-            <MarkdownLink
-              {...linkProps}
-              parentCommentId={parentCommentId}
-              isMarkdownPost={isMarkdownPost}
-              simpleLinks={simpleLinks}
-              allowPreview={isStakeEnoughForPreview(stakedLevel)}
-            />
-          ),
-      // Workaraund of remarkOptions.Fragment
-      div: React.Fragment,
-      img: isStakeEnoughForPreview(stakedLevel)
-        ? ZoomableImage
-        : (imgProps) => (
-            <SimpleImageLink
-              src={imgProps.src}
-              alt={imgProps.alt}
-              title={imgProps.title}
-              helpText={
-                SIMPLE_SITE ? __("This channel isn't staking enough LBRY Credits for inline image previews.") : ''
-              }
-            />
-          ),
-    },
+    remarkReactComponents: {},
   };
 
   const remarkAttrOpts = {
