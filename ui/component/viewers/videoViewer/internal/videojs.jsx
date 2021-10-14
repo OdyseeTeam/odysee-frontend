77889e70a8579567c24af768236eb2e7fa1f6faa
Anthony <contact@anthonymayfield.com>
2021-10-14 17:18:51 +0300

    switch macro

--- ui/component/viewers/videoViewer/internal/videojs.jsx
+++ ui/component/viewers/videoViewer/internal/videojs.jsx
@@ -80,6 +80,8 @@ function hitsFiftyPercent() {
   // from 0 - 999
   const rand = Math.floor(Math.random() * (1000 + 1));
 
+  return true
+
   // 499 is 50% chance of running
   if (rand > 499) {
     return true;
@@ -577,6 +579,32 @@ export default React.memo<Props>(function VideoJs(props: Props) {
         `&vastretry=2` +
         `&hidecontrols=false`;
 
+      const timestamp = new Date().toISOString();
+
+      const videoElement = document.getElementsByClassName('vjs-tech')[0];
+
+      const height = videoElement.offsetHeight;
+      const width = videoElement.offsetWidth;
+
+      const macroUrl1 = 'https://gov.aniview.com/api/adserver/vast3/' +
+        '?AV_PUBLISHERID=60afcbc58cfdb065440d2426' +
+        '&AV_CHANNELID=60b354389c7adb506d0bd9a4' +
+        `&AV_URL=${encodeURIComponent(window.location.href)}` +
+        `&cb=${encodeURIComponent(timestamp)}` +
+        `&AV_WIDTH=${width}` +
+        `&AV_HEIGHT=${height}` +
+        // '&AV_SCHAIN=[SCHAIN_MACRO]' +
+        // '&AV_CCPA=[CCPA_MACRO]' +
+        // '&AV_GDPR=[GDPR_MACRO]' +
+        // '&AV_CONSENT=[CONSENT_MACRO]' +
+        '&usevslot=true';
+        // `&skip=true` +
+        // `&skiptimer=5` +
+        // `&logo=true` +
+        // `&usevslot=true` +
+        // `&vastretry=2` +
+        // `&hidecontrols=false`;
+
       // always have ads on if internal feature is on,
       // otherwise if not authed, roll for 20% to see an ad
       const shouldShowAnAd = internalFeatureEnabled || (allowPreRoll && hitsFiftyPercent());
@@ -590,10 +618,10 @@ export default React.memo<Props>(function VideoJs(props: Props) {
       if (shouldShowAnAd && browserIsChrome && !IS_MOBILE) {
         // fire up ima integration via module
         player.ima({
-          adTagUrl: macroUrl,
-          vpaidMode: 2, // 2 = INSECURE
-          // // $FlowFixMe
-          // vpaidMode: google.ima.ImaSdkSettings.VpaidMode.INSECURE,
+          adTagUrl: macroUrl1,
+          // $FlowFixMe
+          // eslint-disable-next-line no-undef
+          vpaidMode: 2,
         });
       }
 
