707224a0e734569fed86ce0c9d6d952fdd436a2c
Dan Peterson <dan@dan-peterson.ca>
2021-10-20 13:24:28 -0500

    revert dispatch to type any. (linting issues)

--- flow-typed/redux.js
+++ flow-typed/redux.js
@@ -2,6 +2,5 @@
 
 /* eslint-disable no-use-before-define */
 declare type GetState = () => any;
-declare type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
-declare type Dispatch = (action: {} | Promise<*> | Array<{}> | ThunkAction) => any; // Need to refer to ThunkAction
+declare type Dispatch = any;
 /* eslint-enable */
