a63472910f889e0503531bc5944bb36381b5ab86
Dan Peterson <dan@dan-peterson.ca>
2021-10-20 13:12:15 -0500

    merge types from deleted files

--- flow-typed/redux.js
+++ flow-typed/redux.js
@@ -1,3 +1,7 @@
 // @flow
 
-declare type Dispatch = any;
+/* eslint-disable no-use-before-define */
+declare type GetState = () => any;
+declare type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+declare type Dispatch = (action: {} | Promise<*> | Array<{}> | ThunkAction) => any; // Need to refer to ThunkAction
+/* eslint-enable */
