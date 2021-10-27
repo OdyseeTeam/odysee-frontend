645a4e4cfd0b55711e10e64dfdb6602c1b31a12f
Dan Peterson <dan@dan-peterson.ca>
2021-10-27 14:43:28 -0500

    Update import (type/linting issue)

--- web/component/browserNotificationSettings/view.jsx
+++ web/component/browserNotificationSettings/view.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import * as React from 'react';
 import * as ICONS from 'constants/icons';
 import SettingsRow from 'component/settingsRow';
 import { FormField } from 'component/common/form';
