3a06e146e7d3e218000a97f86535e892d65f0823
Dan Peterson <dan@dan-peterson.ca>
2021-11-03 11:03:39 -0500

    update livestream API endpoint

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -2,4 +2,4 @@ export const LIVESTREAM_EMBED_URL = 'https://player.live.odysee.com/odysee';
 export const LIVESTREAM_LIVE_API = 'https://api.live.odysee.com/v1/odysee/live';
 export const LIVESTREAM_REPLAY_API = 'https://api.live.odysee.com/v1/replays/odysee';
 export const LIVESTREAM_RTMP_URL = 'rtmp://stream.odysee.com/live';
-export const LIVESTREAM_KILL = 'http://stream.odysee.com:3000/stream/kill';
+export const LIVESTREAM_KILL = 'https://api.stream.odysee.com/stream/kill';
