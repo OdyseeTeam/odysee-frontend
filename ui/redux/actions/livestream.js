86f2c29ff413ffea13eb5425c3cbb0265f5f6aa9
Dan Peterson <dan@dan-peterson.ca>
2021-12-07 14:31:26 -0600

    update livestream fetching to show scheduled as live when appropriate

--- ui/redux/actions/livestream.js
+++ ui/redux/actions/livestream.js
@@ -36,6 +36,77 @@ export const doFetchNoSourceClaims = (channelId: string) => async (dispatch: Dis
 
 const FETCH_ACTIVE_LIVESTREAMS_MIN_INTERVAL_MS = 5 * 60 * 1000;
 
+const transformLivestreamData = (data: any): LivestreamInfo => {
+  return data.reduce((acc, curr) => {
+    acc[curr.claimId] = {
+      live: curr.live,
+      viewCount: curr.viewCount,
+      creatorId: curr.claimId,
+      startedStreaming: moment(curr.timestamp),
+    };
+    return acc;
+  }, {});
+};
+
+const fetchLiveStreamData = async () => {
+  const response = await fetch(LIVESTREAM_LIVE_API);
+  const json = await response.json();
+  if (!('data' in json)) throw new Error();
+  return transformLivestreamData(json.data);
+};
+
+const filterUpcomingLiveStreamClaims = (upcomingClaims) => {
+  const startsSoonMoment = moment().startOf('minute').add(LIVESTREAM_STARTS_SOON_BUFFER, 'minutes');
+  // $FlowFixMe
+  return Object.values(upcomingClaims).filter((claim) =>
+    // $FlowFixMe
+    moment.unix(claim.stream.value.release_time).isSameOrBefore(startsSoonMoment)
+  );
+};
+
+const fetchUpcomingLivestreamClaims = (channelIds: Array<string>, nextOptions: any) => {
+  return doClaimSearch({
+    page: 1,
+    page_size: nextOptions.page_size,
+    has_no_source: true,
+    channel_ids: channelIds,
+    claim_type: ['stream'],
+    order_by: ['^release_time'],
+    release_time: `>${moment().subtract(5, 'minutes').unix()}`,
+    limit_claims_per_channel: 1,
+    no_totals: true,
+  });
+};
+
+const fetchMostRecentLivestreamClaims = (channelIds: Array<string>, nextOptions: any) => {
+  return doClaimSearch({
+    page: 1,
+    page_size: nextOptions.page_size,
+    has_no_source: true,
+    channel_ids: channelIds,
+    claim_type: ['stream'],
+    order_by: nextOptions.order_by, // **
+    release_time: `<${moment().unix()}`,
+    limit_claims_per_channel: 1, // **
+    no_totals: true,
+  });
+};
+
+const useUpcomingClaim = (channelId: string, claim: any, startingSoonClaims: any, activeLivestreams: any) => {
+  // Determine if this channel has a scheduled / upcoming livestream claim.
+  const upcomingClaim = startingSoonClaims.find((claim) => claim.stream.signing_channel.claim_id === channelId);
+  if (!upcomingClaim) return;
+
+  // Determine if this scheduled livestream should be considered "live" based on when the channel started streaming.
+  const channelStartedStreaming = activeLivestreams[upcomingClaim.stream.signing_channel.claim_id].startedStreaming;
+  const mostRecentDistance = Math.abs(moment.unix(claim.stream.value.release_time).diff(channelStartedStreaming));
+  const upcomingDistance = Math.abs(moment.unix(upcomingClaim.stream.value.release_time).diff(channelStartedStreaming));
+  const useUpcoming = upcomingDistance <= mostRecentDistance;
+
+  if (!useUpcoming) return;
+  return upcomingClaim;
+};
+
 export const doFetchActiveLivestreams = (
   orderBy: Array<string> = ['release_time'],
   pageSize: number = 50,
@@ -57,100 +128,46 @@ export const doFetchActiveLivestreams = (
 
     dispatch({ type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_STARTED });
 
-    // Find all channels that are currently broadcasting a live stream.
-    fetch(LIVESTREAM_LIVE_API)
-      .then((res) => res.json())
-      .then((res) => {
-        if (!res.data) {
-          dispatch({ type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_FAILED });
-          return;
-        }
+    try {
+      const activeLivestreams = await fetchLiveStreamData();
 
-        const activeLivestreams: LivestreamInfo = res.data.reduce((acc, curr) => {
-          acc[curr.claimId] = {
-            live: curr.live,
-            viewCount: curr.viewCount,
-            creatorId: curr.claimId,
-          };
-          return acc;
-        }, {});
+      // Find the most recent claims for the channels that are actively broadcasting a stream.
+      // These newest claims are considered the "live" ones.
+      const mostRecentClaims = await dispatch(
+        fetchMostRecentLivestreamClaims(Object.keys(activeLivestreams), nextOptions)
+      );
 
-        // Find the first upcoming claim (if one exists) for each channel that has been determined to be actively broadcasting a stream.
-        // If the claim is scheduled to "start soon" we'll show it instead of the most recent livestream claim.
-        // @Note: We'll manually filter out any that aren't "starting soon" and ideally in the future the API will allow querying by a date range.
-        const searchUpcomingClaims = dispatch(
-          doClaimSearch({
-            page: 1,
-            page_size: nextOptions.page_size,
-            has_no_source: true,
-            channel_ids: Object.keys(activeLivestreams),
-            claim_type: ['stream'],
-            order_by: ['^release_time'],
-            release_time: `>${moment().subtract(5, 'minutes').unix()}`,
-            limit_claims_per_channel: 1,
-            no_totals: true,
-          })
-        );
+      // Find the first upcoming claim (if one exists) for each channel that's actively broadcasting a stream.
+      const upcomingClaims = await dispatch(fetchUpcomingLivestreamClaims(Object.keys(activeLivestreams), nextOptions));
 
-        // Find the most recent claims for the channels that have been determined to be actively broadcasting a stream.
-        // These newest claims are considered the "live" ones.
-        const searchMostRecentClaims = dispatch(
-          doClaimSearch({
-            page: 1,
-            page_size: nextOptions.page_size,
-            has_no_source: true,
-            channel_ids: Object.keys(activeLivestreams),
-            claim_type: ['stream'],
-            order_by: nextOptions.order_by, // **
-            release_time: `<${moment().unix()}`,
-            limit_claims_per_channel: 1, // **
-            no_totals: true,
-          })
-        );
+      // Filter out any upcoming claims that aren't scheduled to start within the configured buffer time.
+      const startingSoonClaims = filterUpcomingLiveStreamClaims(upcomingClaims);
 
-        Promise.all([searchUpcomingClaims, searchMostRecentClaims])
-          .then(([upcomingClaims, mostRecentClaims]) => {
-            const startsSoonMoment = moment().startOf('minute').add(LIVESTREAM_STARTS_SOON_BUFFER, 'minutes');
+      Object.values(mostRecentClaims).forEach((mostRecentClaim) => {
+        // $FlowFixMe
+        const channelId = mostRecentClaim.stream.signing_channel.claim_id;
+        const claim =
+          useUpcomingClaim(channelId, mostRecentClaim, startingSoonClaims, activeLivestreams) || mostRecentClaim;
 
-            const startingSoonClaims = Object.values(upcomingClaims).filter((claim) => {
-              // $FlowFixMe
-              return moment(claim.stream.value.release_time * 1000).isSameOrBefore(startsSoonMoment);
-            });
-
-            Object.values(mostRecentClaims).forEach((mostRecentClaim) => {
-              // $FlowFixMe
-              const channelId = mostRecentClaim.stream.signing_channel.claim_id;
-              const upcomingClaim = startingSoonClaims.find((claim) => {
-                // $FlowFixMe
-                return claim.stream.signing_channel.claim_id === channelId;
-              });
-
-              const claim = upcomingClaim || mostRecentClaim;
-
-              activeLivestreams[channelId] = {
-                ...activeLivestreams[channelId],
-                // $FlowFixMe
-                latestClaimId: claim.stream.claim_id,
-                // $FlowFixMe
-                latestClaimUri: claim.stream.canonical_url,
-              };
-            });
-
-            dispatch({
-              type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_COMPLETED,
-              data: {
-                activeLivestreams,
-                activeLivestreamsLastFetchedDate: now,
-                activeLivestreamsLastFetchedOptions: nextOptions,
-              },
-            });
-          })
-          .catch(() => {
-            dispatch({ type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_FAILED });
-          });
-      })
-      .catch((err) => {
-        dispatch({ type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_FAILED });
+        activeLivestreams[channelId] = {
+          ...activeLivestreams[channelId],
+          // $FlowFixMe
+          latestClaimId: claim.stream.claim_id,
+          // $FlowFixMe
+          latestClaimUri: claim.stream.canonical_url,
+        };
       });
+
+      dispatch({
+        type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_COMPLETED,
+        data: {
+          activeLivestreams,
+          activeLivestreamsLastFetchedDate: now,
+          activeLivestreamsLastFetchedOptions: nextOptions,
+        },
+      });
+    } catch {
+      dispatch({ type: ACTIONS.FETCH_ACTIVE_LIVESTREAMS_FAILED });
+    }
   };
 };
