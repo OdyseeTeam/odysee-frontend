5bd736490c773d2184ccf2ab24ebd04d9dc46687
Anthony <contact@anthonymayfield.com>
2021-12-08 17:34:22 +0100

    remove double call to fetch source

--- ui/component/viewers/videoViewer/internal/videojs.jsx
+++ ui/component/viewers/videoViewer/internal/videojs.jsx
@@ -243,6 +243,11 @@ export default React.memo<Props>(function VideoJs(props: Props) {
       playerRef.current = vjsPlayer;
 
       window.addEventListener('keydown', curried_function(playerRef, containerRef));
+
+      // PR #5570: Temp workaround to avoid double Play button until the next re-architecture.
+      if (!window.player.paused()) {
+        (window.player.bigPlayButton && window.player.bigPlayButton.hide());
+      }
     });
 
     // Cleanup
@@ -255,41 +260,7 @@ export default React.memo<Props>(function VideoJs(props: Props) {
         window.player = undefined;
       }
     };
-  }, [isAudio, source]);
-
-  // Update video player and reload when source URL changes
-  useEffect(() => {
-    // For some reason the video player is responsible for detecting content type this way
-    fetch(source, { method: 'HEAD', cache: 'no-store' }).then((response) => {
-      let finalType = sourceType;
-      let finalSource = source;
-
-      // override type if we receive an .m3u8 (transcoded mp4)
-      // do we need to check if explicitly redirected
-      // or is checking extension only a safer method
-      if (response && response.redirected && response.url && response.url.endsWith('m3u8')) {
-        finalType = 'application/x-mpegURL';
-        finalSource = response.url;
-      }
-
-      // Modify video source in options
-      videoJsOptions.sources = [
-        {
-          src: finalSource,
-          type: finalType,
-        },
-      ];
-
-      // Update player source
-      const player = playerRef.current;
-      if (!player) return;
-
-      // PR #5570: Temp workaround to avoid double Play button until the next re-architecture.
-      if (!player.paused()) {
-        player.bigPlayButton.hide();
-      }
-    });
-  }, [source, reload]);
+  }, [isAudio, source, reload]);
 
   return (
     <div className={classnames('video-js-parent', { 'video-js-parent--ios': IS_IOS })} ref={containerRef}>
