344e38fe9d0b7b5c4039ec6283e6b7f9b9e09222
toshokanneko <raphael.wickihalder@protonmail.com>
2021-12-11 14:17:52 +0100

    Fix empty ratio bar

--- ui/component/ratioBar/view.jsx
+++ ui/component/ratioBar/view.jsx
@@ -10,14 +10,16 @@ const RatioBar = (props: Props) => {
   const { likeCount, dislikeCount } = props;
 
   const like = (1 / (likeCount + dislikeCount)) * likeCount;
-  return (
-    like && (
+  if (like) {
+    return (
       <div className={'ratio-bar'}>
         <div className={'ratio-bar-like'} style={{ flex: like }} />
         <div className={'ratio-bar-dislike'} style={{ flex: 1 - like }} />
       </div>
-    )
-  );
+    );
+  } else {
+    return <div className={'ratio-bar'} />;
+  }
 };
 
 export default RatioBar;
