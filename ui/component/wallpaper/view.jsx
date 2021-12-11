a5f60e20bc1a8afbeb05eb0508d6833c96bfd807
toshokanneko <raphael.wickihalder@protonmail.com>
2021-12-11 23:09:01 +0100

    Update gray shade algorithm

--- ui/component/wallpaper/view.jsx
+++ ui/component/wallpaper/view.jsx
@@ -15,7 +15,7 @@ const Wallpaper = (props: Props) => {
     toDataUrl(avatar, function (image) {
       if (image) {
         getAverageRGB(image, function (rgb) {
-          // let hsl = rgb2hsl(rgb.r, rgb.g, rgb.b);
+          // let hsl = rgb2hsl(rgb);
           let brightness = Math.round((parseInt(rgb.r) * 299 + parseInt(rgb.g) * 587 + parseInt(rgb.b) * 114) / 1000);
           document.documentElement !== null &&
             document.documentElement.style.setProperty('--color-primary-dynamic', rgb.r + ',' + rgb.g + ',' + rgb.b);
@@ -101,14 +101,6 @@ const Wallpaper = (props: Props) => {
     }
 
     length = data.data.length;
-    let primary = {
-      red: 0,
-      purple: 0,
-      blue: 0,
-      turquoise: 0,
-      green: 0,
-      yellow: 0,
-    };
 
     while ((i += blockSize * 4) < length) {
       if (shadeCheck(data.data, i)) {
@@ -116,45 +108,7 @@ const Wallpaper = (props: Props) => {
         rgb.r += data.data[i];
         rgb.g += data.data[i + 1];
         rgb.b += data.data[i + 2];
-
-        let threshold = 80;
-        let threshold_b = 180;
-        if (data.data[i] > threshold_b) {
-          // has red?
-          if ((100 / data.data[i]) * data.data[i + 1] < threshold) {
-            // red without green
-            if ((100 / data.data[i]) * data.data[i + 2] < threshold) primary.red++;
-            // red without green & blue
-            else primary.purple++; // red without green with blue
-          } else primary.yellow++; // red with green
-        }
-        if (data.data[i + 1] > threshold_b) {
-          // has green?
-          if ((100 / data.data[i + 1]) * data.data[i] < threshold) {
-            // green without red
-            if ((100 / data.data[i + 1]) * data.data[i + 2] < threshold) primary.green++;
-            // green without red & blue
-            else primary.turquoise++; // green without red with blue
-          } else primary.yellow++; // green with red
-        }
-        if (data.data[i + 2] > threshold_b) {
-          // has blue?
-          if ((100 / data.data[i + 2]) * data.data[i] < threshold) {
-            // blue without red
-            if ((100 / data.data[i + 2]) * data.data[i + 1] < threshold) primary.blue++;
-            // blue without red & green
-            else primary.turquoise++; // blue without red with green
-          } else primary.purple++; // blue with red
-        }
       }
-    }
-    primary.turquoise = primary.turquoise / 2;
-    primary.purple = primary.purple / 2;
-    let max = 0;
-    let second = 0;
-    for (let i in primary) {
-      if (primary[i] > max) max = primary[i];
-      if (primary[i] > second && primary[i] < max) second = primary[i];
     }
 
     rgb.r = ~~(rgb.r / count);
@@ -163,6 +117,22 @@ const Wallpaper = (props: Props) => {
 
     callback(rgb);
   }
+
+  /*
+  function shadeCheck(data, i) {
+    let threshold = 50;
+    let gray = 0;
+    if (
+      Math.abs(data[i], data[i + 1]) < threshold &&
+      Math.abs(data[i], data[i + 2]) < threshold &&
+      Math.abs(data[i + 1], data[i + 2]) < threshold
+    ) {
+      gray = 1;
+    }
+
+    return gray;
+  }
+*/
 
   function shadeCheck(data, i) {
     let threshold = 55;
