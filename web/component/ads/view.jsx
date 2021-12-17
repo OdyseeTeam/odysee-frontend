57ffbeedd818e54b739737e0c1a700bdd68f24a2
toshokanneko <raphael.wickihalder@protonmail.com>
2021-12-17 21:28:20 +0100

    merge

--- web/component/ads/view.jsx
+++ web/component/ads/view.jsx
@@ -6,17 +6,16 @@ import { withRouter } from 'react-router';
 import I18nMessage from 'component/i18nMessage';
 import Button from 'component/button';
 import classnames from 'classnames';
-// $FlowFixMe
-
-const IS_MOBILE = typeof window.orientation !== 'undefined';
 
 const ADS_URL =
   'https://cdn.vidcrunch.com/integrations/618bb4d28aac298191eec411/Lbry_Odysee.com_Responsive_Floating_DFP_Rev70_1011.js';
 const ADS_TAG = 'vidcrunchJS537102317';
-
 const IOS_ADS_URL =
   'https://cdn.vidcrunch.com/integrations/618bb4d28aac298191eec411/Lbry_Odysee.com_Mobile_Floating_DFP_Rev70_1611.js';
 const IOS_ADS_TAG = 'vidcrunchJS199212779';
+const HOMEPAGE_ADS_URL =
+  'https://cdn.vidcrunch.com/integrations/618bb4d28aac298191eec411/Lbry_Odysee.com_Responsive_Floating_300x169_DFP_Rev70_1211.js';
+const HOMEPAGE_ADS_TAG = 'vidcrunchJS330442776';
 
 const IS_IOS =
   (/iPad|iPhone|iPod/.test(navigator.platform) ||
@@ -24,64 +23,84 @@ const IS_IOS =
     (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
   !window.MSStream;
 
+const IS_ANDROID = /Android/i.test(navigator.userAgent);
+
+const IS_FIREFOX = /Firefox/i.test(navigator.userAgent);
+
+const isFirefoxAndroid = IS_ANDROID && IS_FIREFOX;
+
 type Props = {
   location: { pathname: string },
   type: string,
   small: boolean,
   claim: Claim,
   isMature: boolean,
+  authenticated: boolean,
+  triggerBlacklist: boolean,
 };
+
+function removeIfExists(querySelector) {
+  const element = document.querySelector(querySelector);
+  if (element) element.remove();
+}
 
 function Ads(props: Props) {
   const {
     location: { pathname },
-    type = 'sidebar',
+    type = 'video',
     small,
+    authenticated,
+    triggerBlacklist,
   } = props;
 
+  // load ad and tags here
   let scriptUrlToUse;
   let tagNameToUse;
-  if (IS_IOS) {
-    tagNameToUse = IOS_ADS_TAG;
-    scriptUrlToUse = IOS_ADS_URL;
-  } else {
-    tagNameToUse = ADS_TAG;
-    scriptUrlToUse = ADS_URL;
+  if (type === 'video') {
+    if (IS_IOS) {
+      tagNameToUse = IOS_ADS_TAG;
+      scriptUrlToUse = IOS_ADS_URL;
+    } else {
+      tagNameToUse = ADS_TAG;
+      scriptUrlToUse = ADS_URL;
+    }
+  } else if (type === 'homepage') {
+    tagNameToUse = HOMEPAGE_ADS_TAG;
+    scriptUrlToUse = HOMEPAGE_ADS_URL;
   }
 
+  // add script to DOM
   useEffect(() => {
-    if (SHOW_ADS && type === 'video') {
+    if (SHOW_ADS && !authenticated) {
       let script;
       try {
-        let fjs = document.getElementsByTagName('script')[0];
         script = document.createElement('script');
         script.src = scriptUrlToUse;
         // $FlowFixMe
-        fjs.parentNode.insertBefore(script, fjs);
+        document.head.appendChild(script);
+
+        return () => {
+          // $FlowFixMe
+          document.head.removeChild(script);
+
+          // clear aniview state to allow ad reload
+          delete window.aniplayerPos;
+          delete window.storageAni;
+          delete window.__VIDCRUNCH_CONFIG_618bb4d28aac298191eec411__;
+          delete window.__player_618bb4d28aac298191eec411__;
+
+          // clean DOM elements from ad related elements
+          removeIfExists('[src^="https://cdn.vidcrunch.com/618bb4d28aac298191eec411.js"]');
+          removeIfExists('[src^="https://player.aniview.com/script/6.1/aniview.js"]');
+          removeIfExists('[id^="AVLoaderaniplayer_vidcrunch"]');
+          removeIfExists('#av_css_id');
+          removeIfExists('#customAniviewStyling');
+        };
       } catch (e) {}
     }
-  }, [type]);
+  }, []);
 
-  useEffect(() => {
-    if (SHOW_ADS && !IS_MOBILE && type === 'sidebar') {
-      const script = document.createElement('script');
-      script.src = ADS_URL;
-      script.defer = true;
-      // $FlowFixMe
-      document.body.appendChild(script);
-      return () => {
-        // $FlowFixMe
-        document.body.removeChild(script);
-        // if user navigates too rapidly, <style> tags can build up
-        // $FlowFixMe
-        if (document.body.getElementsByTagName('style').length) {
-          // $FlowFixMe
-          document.body.getElementsByTagName('style')[0].remove();
-        }
-      };
-    }
-  }, [type]);
-
+  // display to say "sign up to not see these"
   const adsSignInDriver = (
     <I18nMessage
       tokens={{
@@ -98,6 +117,7 @@ function Ads(props: Props) {
     </I18nMessage>
   );
 
+  // ad shown in the related videos area
   const videoAd = (
     <div className="ads__claim-item">
       <div id={tagNameToUse} className="ads__injected-video" style={{ display: 'none' }} />
@@ -112,11 +132,26 @@ function Ads(props: Props) {
     </div>
   );
 
-  if (!SHOW_ADS) {
+  // homepage ad in a card
+  const homepageCardAd = (
+    <div className="homepageAdContainer media__thumb" style={{ display: 'none' }}>
+      <div id={tagNameToUse} className="homepageAdDiv media__thumb" style={{ display: 'none' }} />
+    </div>
+  );
+
+  // dont show if ads disabled on instance, blacklist word matched
+  if (!SHOW_ADS || triggerBlacklist) {
     return false;
   }
+  // disable ads for firefox android because they don't work properly
+  if (isFirefoxAndroid) return false;
+
+  // sidebar ad (in recommended videos)
   if (type === 'video') {
     return videoAd;
+  }
+  if (type === 'homepage') {
+    return homepageCardAd;
   }
 }
 
