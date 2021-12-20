63e4adbfce0a1b02b37c8b8d52dadd3622825b9a
Rafael <rafael.saes@odysee.com>
2021-12-20 12:00:01 -0300

    Add German channels

--- ui/constants/community_channels.js
+++ ui/constants/community_channels.js
@@ -5,10 +5,16 @@ const PT_BR_CHANNELS =
   ' ' +
   'lbry://@ajuda#d3a0afbe782c5ad13c944bdf12c1387302868c73';
 
+const DE_CHANNELS =
+  'lbry://@OdyseeDE#1c44ca079e0e3a824881184fdffcf1864cb2649e' +
+  ' ' +
+  'lbry://@OdyseeHilfe#14dd52c6105698159df73eb1fac89da477f895ea';
+
 const COMMUNITY_CHANNELS = {
   en: AUTO_FOLLOW_CHANNELS,
   'pt-BR': PT_BR_CHANNELS,
   es: 'lbry://@ayuda#7385d06a753744996461f5aa30daa570b85bd8d2',
+  de: DE_CHANNELS,
 };
 
 export default COMMUNITY_CHANNELS;
