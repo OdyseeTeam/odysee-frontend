3057c09b970f5a7dd90e7fdfcd30927785583592
Anthony <contact@anthonymayfield.com>
2021-12-22 18:52:28 +0100

    change to functional component

--- ui/page/odyseeMembership/index.js
+++ ui/page/odyseeMembership/index.js
@@ -1,23 +1,12 @@
 import { connect } from 'react-redux';
-import { doSetClientSetting } from 'redux/actions/settings';
-import { selectosNotificationsEnabled } from 'redux/selectors/settings';
-import { selectUserVerifiedEmail, selectUserEmail } from 'redux/selectors/user';
+import { selectTotalBalance } from 'redux/selectors/wallet';
 import { doOpenModal } from 'redux/actions/app';
-import { doToast } from 'redux/actions/notifications';
-
-import SettingsStripeCard from './view';
+import OdyseeMembership from './view';
 
 const select = (state) => ({
-  osNotificationsEnabled: selectosNotificationsEnabled(state),
-  isAuthenticated: Boolean(selectUserVerifiedEmail(state)),
-  email: selectUserEmail(state),
+  totalBalance: selectTotalBalance(state),
 });
 
-const perform = (dispatch) => ({
-  setClientSetting: (key, value) => dispatch(doSetClientSetting(key, value)),
+export default connect(select, {
   doOpenModal,
-  openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
-  doToast: (options) => dispatch(doToast(options)),
-});
-
-export default connect(select, perform)(SettingsStripeCard);
+})(OdyseeMembership);
