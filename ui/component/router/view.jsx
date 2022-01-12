a417619700ca6f77049bd06a17bea9253d238a01
infinite-persistence <inf.persistence@gmail.com>
2022-01-12 11:58:37 +0800

    Fix direct access to private routes not redirecting back

--- ui/component/router/view.jsx
+++ ui/component/router/view.jsx
@@ -157,12 +157,7 @@ function PrivateRoute(props: PrivateRouteProps) {
         (keycloak && keycloak.authenticated) || !IS_WEB ? (
           <Component {...props} />
         ) : (
-          <Redirect
-            to={{
-              pathname: `/$/${PAGES.AUTH_SIGNIN}?redirect=${redirectUrl || props.location.pathname}`,
-              state: { from: props.location },
-            }}
-          />
+          <Redirect to={`/$/${PAGES.AUTH_SIGNIN}?redirect=${redirectUrl || props.location.pathname}`} />
         )
       }
     />
@@ -329,6 +324,7 @@ function AppRouter(props: Props) {
         {categoryPages}
 
         <Route path={`/$/${PAGES.AUTH_SIGNIN}`} exact component={SignInPage} />
+        <Route path={`/$/${PAGES.AUTH_SIGNIN}/*`} exact component={SignInPage} />
         <Route path={`/$/${PAGES.AUTH_PASSWORD_RESET}`} exact component={PasswordResetPage} />
         <Route path={`/$/${PAGES.AUTH_PASSWORD_SET}`} exact component={PasswordSetPage} />
         <Route path={`/$/${PAGES.AUTH}`} exact component={SignUpPage} />
