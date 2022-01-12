ce3ca18509a16b3fc68437cc98a02412031d7357
Anthony <contact@anthonymayfield.com>
2022-01-12 21:44:08 +0100

    building out frontend

--- ui/page/odyseeMembership/view.jsx
+++ ui/page/odyseeMembership/view.jsx
@@ -15,40 +15,69 @@ type Props = {
 };
 
 const OdyseeMembershipPage = (props: Props) => {
-
-  console.log(props);
+  // console.log(props);
 
   const [cardSaved, setCardSaved] = React.useState();
   const [membershipOptions, setMembershipOptions] = React.useState();
+  const [userMembership, setUserMemberships] = React.useState();
 
-  React.useEffect(function(){
-    // check if there is a payment method
-    Lbryio.call('customer', 'status', {
-      environment: stripeEnvironment,
-    }, 'post').then(function(response){
-      console.log('status');
-      console.log(response);
-      // hardcoded to first card
-      setCardSaved(response && response.PaymentMethods && response.PaymentMethods[0]);
-    });
+  React.useEffect(function() {
+    (async function() {
+      try {
+        // check if there is a payment method
+        const response = await Lbryio.call('customer', 'status', {
+          environment: stripeEnvironment,
+        }, 'post');
+        console.log('status (if there is a payment methods');
+        console.log(response);
+        // hardcoded to first card
+        setCardSaved(response && response.PaymentMethods && response.PaymentMethods[0]);
+      } catch (err) {
+        console.log(err);
+      }
 
-    // check the available payment methods
-    Lbryio.call('membership', 'list', {
-      environment: stripeEnvironment,
-      channel_id: odyseeChannelId,
-      channel_name: odyseeChannelName,
-    }, 'post').then(function(response){
-      console.log('membership');
-      console.log(response);
-      setMembershipOptions(response);
-    });
+      try {
+        // check the available membership for odysee.com
+        const response = await Lbryio.call('membership', 'list', {
+          environment: stripeEnvironment,
+          channel_id: odyseeChannelId,
+          channel_name: odyseeChannelName,
+        }, 'post');
 
-    Lbryio.call('membership', 'mine', {
-      environment: stripeEnvironment,
-    }, 'post').then(function(response){
-      console.log('mine');
-      console.log(response);
-    });
+        console.log('list, see all the available odysee memberships');
+        console.log(response);
+        setMembershipOptions(response);
+      } catch (err) {
+        console.log(err);
+      }
+
+      try {
+        // show the memberships the user is subscribed to
+        const response = await Lbryio.call('membership', 'mine', {
+          environment: stripeEnvironment,
+        }, 'post');
+
+        console.log('mine, my subscriptions');
+        console.log(response);
+        setUserMemberships(response);
+      } catch (err) {
+        console.log(err);
+      }
+
+      // try {
+      //   // show the memberships the user is subscribed to
+      //   const response = await Lbryio.call('membership', 'cancel', {
+      //     environment: stripeEnvironment,
+      //     membership_id: 2,
+      //   }, 'post');
+      //
+      //   console.log('cancel, cancel membership response');
+      //   console.log(response);
+      //
+      // } catch (err) {
+      //   console.log(err);
+      // }
+    })();
   }, []);
 
   React.useEffect(function(){
@@ -74,13 +103,28 @@ const OdyseeMembershipPage = (props: Props) => {
   return (
     <>
       <Page>
-        <h1>Odysee Memberships</h1>
+        <h1 style={{fontSize: '19px'}}>Odysee Memberships</h1>
         {cardSaved && membershipOptions && (
           <div>
+            <h1 style={{marginTop: '17px', fontSize: '16px' }}>Available Memberships:</h1>
             { membershipOptions.map((membershipOption) => (
               <>
-                <div style={{ 'margin-top': '5px';}}>
-                  {membershipOption.type}
+                <div style={{ 'margin-top': '16px', marginBottom: '10px'}}>
+                  <h4 style={{marginBottom: '3px'}}>Name: {membershipOption.name}</h4>
+                  <h4 style={{marginBottom: '3px'}}>Description: {membershipOption.description}</h4>
+                  { membershipOption.type === 'yearly' && (
+                    <>
+                      <h4 style={{marginBottom: '4px'}}>Subscription Period Options: Yearly</h4>
+                      <h4 style={{marginBottom: '4px'}}>${(membershipOption.cost_usd * 12) / 100 } USD For A One Year Subscription (${membershipOption.cost_usd / 100} Per Month)</h4>
+                    </>
+                  )}
+                  { membershipOption.type === 'both' && (
+                    <>
+                      <h4 style={{marginBottom: '4px'}}>Subscription Period Options: Yearly And Monthly</h4>
+                      <h4 style={{marginBottom: '4px'}}>${(membershipOption.cost_usd * 12) / 100 } USD For A One Year Subscription (${membershipOption.cost_usd / 100} Per Month)</h4>
+                      <h4 style={{marginBottom: '4px'}}>${(membershipOption.cost_usd) / 100 } USD Per Month For A Monthly Renewing Subscription)</h4>
+                    </>
+                  )}
                 </div>
               </>
             ))}
