6647f226b86d8cdfbf3eec98a191ab6776bb5a5f
Rafael <rafael.saes@odysee.com>
2022-01-24 13:41:56 -0300

    adding modal to memberships

--- ui/modal/modalRouter/view.jsx
+++ ui/modal/modalRouter/view.jsx
@@ -65,6 +65,7 @@ const ModalPublishPreview = lazyImport(() =>
   import('modal/modalPublishPreview' /* webpackChunkName: "modalPublishPreview" */)
 );
 const ModalRemoveCard = lazyImport(() => import('modal/modalRemoveCard' /* webpackChunkName: "modalRemoveCard" */));
+const ModalConfirmPurchaseOdyseeMembership = lazyImport(() => import('modal/modalConfirmPurchaseOdyseeMembership' /* webpackChunkName: "modalConfirmPurchaseOdyseeMembership" */));
 const ModalRemoveComment = lazyImport(() =>
   import('modal/modalRemoveComment' /* webpackChunkName: "modalRemoveComment" */)
 );
@@ -180,6 +181,8 @@ function getModal(id) {
       return ModalDeleteCollection;
     case MODALS.CONFIRM_REMOVE_CARD:
       return ModalRemoveCard;
+    case MODALS.CONFIRM_PURCHASE_ODYSEE_MEMBERSHIP:
+      return ModalConfirmPurchaseOdyseeMembership;
     case MODALS.CONFIRM_REMOVE_COMMENT:
       return ModalRemoveComment;
     default:
