10d875d78cf04dd43387f9756229db8d0be8c80a
Anthony <contact@anthonymayfield.com>
2022-01-26 00:07:43 +0100

    add premium icon

--- ui/constants/icons.js
+++ ui/constants/icons.js
@@ -186,3 +186,4 @@ export const POP_CULTURE = 'Pop Culture';
 export const ODYSEE_LOGO = 'OdyseeLogo';
 export const ODYSEE_WHITE_TEXT = 'OdyseeLogoWhiteText';
 export const ODYSEE_DARK_TEXT = 'OdyseeLogoDarkText';
+export const PREMIUM_PLUS_BADGE = 'PremiumPlusBadge';
