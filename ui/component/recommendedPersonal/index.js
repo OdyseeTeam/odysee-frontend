31f627281ca723d9f60986e63063a9fdaed7c82c
infinite-persistence <inf.persistence@gmail.com>
2022-01-26 10:41:34 +0800

    Mark rendered FYPs

--- ui/component/recommendedPersonal/index.js
+++ ui/component/recommendedPersonal/index.js
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux';
 import { doFetchPersonalRecommendations } from 'redux/actions/search';
 import { selectPersonalRecommendations } from 'redux/selectors/search';
+import { selectUser } from 'redux/selectors/user';
 
 import RecommendedPersonal from './view';
 
-const select = (state) => ({
-  personalRecommendations: selectPersonalRecommendations(state),
-});
+const select = (state) => {
+  const user = selectUser(state);
+  return {
+    userId: user && user.id,
+    personalRecommendations: selectPersonalRecommendations(state),
+  };
+};
 
 const perform = {
   doFetchPersonalRecommendations,
