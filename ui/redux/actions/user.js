46e07a5015960e0ee3b0d3b8ee3aab67f9c7af99
infinite-persistence <inf.persistence@gmail.com>
2022-02-10 16:37:18 +0800

    ---TMP: put back in the web bypass to reproduce the issue for Tom

--- ui/redux/actions/user.js
+++ ui/redux/actions/user.js
@@ -203,7 +203,7 @@ export function doAuthenticate(
         if (shareUsageData) {
           dispatch(doRewardList());
 
-          if (callInstall) {
+          if (callInstall && !user?.device_types?.includes('web')) {
             doInstallNew(appVersion, callbackForUsersWhoAreSharingData, DOMAIN);
           }
         }
