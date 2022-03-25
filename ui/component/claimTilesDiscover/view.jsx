c6e6b151d54880db5fd00dac2e373329a7d326da
infinite-persistence <inf.persistence@gmail.com>
2022-03-25 14:36:24 +0800

    Minor cleanup, no functional change
    - Self-documenting variable.
    - Remove incorrect comments.
    - Remove redundant variables.

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -79,10 +79,8 @@ function ClaimTilesDiscover(props: Props) {
     doFetchUserMemberships,
   } = props;
 
-  // reference to the claim-grid
-  const sectionRef = React.useRef();
-  // determine the index where the ad should be injected
-  const injectedIndex = useLastVisibleItem(injectedItem, sectionRef);
+  const listRef = React.useRef();
+  const injectedIndex = useLastVisibleItem(injectedItem, listRef);
 
   const prevUris = React.useRef();
   const claimSearchUris = claimSearchResults || [];
@@ -117,12 +115,9 @@ function ClaimTilesDiscover(props: Props) {
   // --------------------------------------------------------------------------
   // --------------------------------------------------------------------------
 
-  // populate the view counts for the current claim uris
   useFetchViewCount(fetchViewCount, uris, claimsByUri, doFetchViewCount);
 
-  const shouldFetchUserMemberships = true;
-
-  useGetUserMemberships(shouldFetchUserMemberships, uris, claimsByUri, doFetchUserMemberships);
+  useGetUserMemberships(true, uris, claimsByUri, doFetchUserMemberships);
 
   // Run `doClaimSearch`
   React.useEffect(() => {
@@ -133,11 +128,10 @@ function ClaimTilesDiscover(props: Props) {
   }, [doClaimSearch, shouldPerformSearch, optionsStringified]);
 
   return (
-    <ul ref={sectionRef} className="claim-grid">
+    <ul ref={listRef} className="claim-grid">
       {finalUris && finalUris.length
         ? finalUris.map((uri, i) => {
             if (uri) {
-              // if indexes match, inject ad in place of tile (aka replace it)
               if (injectedIndex === i && injectedItem && injectedItem.replace) {
                 return <React.Fragment key={uri}>{injectedItem.node}</React.Fragment>;
               }
@@ -145,7 +139,6 @@ function ClaimTilesDiscover(props: Props) {
               return (
                 <React.Fragment key={uri}>
                   {injectedIndex === i && injectedItem && injectedItem.node}
-                  {/* inject ad */}
                   <ClaimPreviewTile
                     showNoSourceClaims={hasNoSource || showNoSourceClaims}
                     uri={uri}
