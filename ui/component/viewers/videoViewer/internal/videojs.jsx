bf6a4b270599b783b5f8f66fa4194ad772f9e798
myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-05-08 18:01:45 +0200

    created function

--- ui/component/viewers/videoViewer/internal/videojs.jsx
+++ ui/component/viewers/videoViewer/internal/videojs.jsx
@@ -26,6 +26,7 @@ import videojs from 'video.js';
 import { useIsMobile } from 'effects/use-screensize';
 import { platform } from 'util/platform';
 import usePersistedState from 'effects/use-persisted-state';
+import addVolumeMouseWheelListener from './volume-mousewheel-listener';
 
 const canAutoplay = require('./plugins/canAutoplay');
 
@@ -336,6 +337,12 @@ export default React.memo<Props>(function VideoJs(props: Props) {
 
       Chromecast.initialize(player);
       player.airPlay();
+
+      addVolumeMouseWheelListener(
+        el,
+        () => console.log(player),
+        () => null
+      );
     });
 
     // fixes #3498 (https://github.com/lbryio/lbry-desktop/issues/3498)
