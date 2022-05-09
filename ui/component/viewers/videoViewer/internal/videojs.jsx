0ece9c9baad092a73373a2f4636d8a48c82be98c
myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-05-09 17:23:43 +0200

    removed console logs

--- ui/component/viewers/videoViewer/internal/videojs.jsx
+++ ui/component/viewers/videoViewer/internal/videojs.jsx
@@ -340,7 +340,7 @@ export default React.memo<Props>(function VideoJs(props: Props) {
 
       addVolumeMouseWheelListener(
         el,
-        () => console.log(player),
+        () => null,
         () => null
       );
     });
