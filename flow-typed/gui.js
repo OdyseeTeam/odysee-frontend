c67c524b36e4be85a52e9083b93526b65f130b6c
infinite-persistence <inf.persistence@gmail.com>
2022-05-30 16:37:02 +0800

    Injection: add ability to bypass count compensation

--- flow-typed/gui.js
+++ flow-typed/gui.js
@@ -1,5 +1,10 @@
 
+declare type ListInjectedItemCallback = (index: number, lastVisibleIndex: ?number, pageSize: ?number) => Node;
+
 declare type ListInjectedItem = {
-  node: Node | (index: number, lastVisibleIndex: ?number, pageSize: ?number) => Node,
-  index?: number,
+  node: Node | ListInjectedItemCallback, // The element to inject, or a callback to retrieve the element(s) to inject.
+  index?: number, // The location to inject the item. Not applicable in callback-mode.
+  disableCountCompensation?: boolean, // By default, the list compensates (reduces) the number of items shown per page
+                                      // to ensure the original item-per-page count stays the same. This flag disables
+                                      // that -- useful if the injected item is a css-block, or for whatever reason.
 };
