dbc44c729ef0fce4cc4bce2ebfa30343d58c3f57
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-06-17 13:01:18 +0200

    Fix header menu links

--- ui/component/headerMenuButtons/view.jsx
+++ ui/component/headerMenuButtons/view.jsx
@@ -2,12 +2,10 @@
 import 'scss/component/_header.scss';
 
 import { ENABLE_NO_SOURCE_CLAIMS } from 'config';
-import { Menu, MenuList, MenuButton } from '@reach/menu-button';
 import { useHistory } from 'react-router';
 import * as ICONS from 'constants/icons';
 import * as PAGES from 'constants/pages';
 import Button from 'component/button';
-import HeaderMenuLink from 'component/common/header-menu-link';
 import Icon from 'component/common/icon';
 import React from 'react';
 import Tooltip from 'component/common/tooltip';
@@ -27,30 +25,31 @@ export default function HeaderMenuButtons(props: HeaderMenuButtonProps) {
   const uploadProps = { requiresAuth: !authenticated };
   const { push } = useHistory();
 
-  return (
+  return authenticated ? (
     <div className="header__buttons">
-      <Tooltip title={__('Go live')}>
-        <Button className="header__navigationItem--icon">
-          <Icon size={18} icon={ICONS.VIDEO} aria-hidden />
-        </Button>
-      </Tooltip>
+      {livestreamEnabled && (
+        <Tooltip title={__('Go live')}>
+          <Button className="header__navigationItem--icon" {...uploadProps} navigate={`/$/${PAGES.LIVESTREAM}`}>
+            <Icon size={18} icon={ICONS.VIDEO} aria-hidden />
+          </Button>
+        </Tooltip>
+      )}
       <Tooltip title={__('Upload a file')}>
-        <Button className="header__navigationItem--icon">
+        <Button className="header__navigationItem--icon" navigate={`/$/${PAGES.UPLOAD}`}>
           <Icon size={18} icon={ICONS.PUBLISH} aria-hidden />
         </Button>
       </Tooltip>
       <Tooltip title={__('Post an article')}>
-        <Button className="header__navigationItem--icon">
+        <Button className="header__navigationItem--icon" navigate={`/$/${PAGES.POST}`}>
           <Icon size={18} icon={ICONS.POST} aria-hidden />
         </Button>
       </Tooltip>
-      {!authenticated && (
-        <Tooltip title={__('Settings')}>
-          <Button className="header__navigationItem--icon" onClick={() => push(`/$/${PAGES.SETTINGS}`)}>
-            <Icon size={18} icon={ICONS.SETTINGS} aria-hidden />
-          </Button>
-        </Tooltip>
-      )}
     </div>
+  ) : (
+    <Tooltip title={__('Settings')}>
+      <Button className="header__navigationItem--icon" onClick={() => push(`/$/${PAGES.SETTINGS}`)}>
+        <Icon size={18} icon={ICONS.SETTINGS} aria-hidden />
+      </Button>
+    </Tooltip>
   );
 }
