7bd4d57255096b1c23f7ab6f239e5709f8df38dd
Rafael <rafael.saes@odysee.com>
2022-06-20 17:28:11 -0300

    Improve /$/playlists page visibility and channel display

--- ui/page/playlists/internal/collectionsListMine/internal/collectionPreview/internal/collection-public-icon.jsx
+++ ui/page/playlists/internal/collectionsListMine/internal/collectionPreview/internal/collection-public-icon.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import * as ICONS from 'constants/icons';
 import Icon from 'component/common/icon';
 
-const CollectionPrivateIcon = () => (
+const CollectionPublicIcon = () => (
   <div className="claim-preview__overlay-properties--small visibility-icon">
-    <Icon icon={ICONS.LOCK} />
-    <span>{__('Private')}</span>
+    <Icon icon={ICONS.EYE} />
+    <span>{__('Public')}</span>
   </div>
 );
 
-export default CollectionPrivateIcon;
+export default CollectionPublicIcon;
--- ui/page/playlists/internal/collectionsListMine/internal/collectionPreview/view.jsx
+++ ui/page/playlists/internal/collectionsListMine/internal/collectionPreview/view.jsx
@@ -4,6 +4,7 @@ import { NavLink, useHistory } from 'react-router-dom';
 import TruncatedText from 'component/common/truncated-text';
 import CollectionItemCount from './internal/collection-item-count';
 import CollectionPrivateIcon from './internal/collection-private-icon';
+import CollectionPublicIcon from './internal/collection-public-icon';
 import CollectionMenuList from 'component/collectionMenuList';
 import * as PAGES from 'constants/pages';
 import * as ICONS from 'constants/icons';
@@ -95,30 +96,33 @@ function CollectionPreview(props: Props) {
                 </h2>
               </NavLink>
 
+              {hasClaim && (
+                <div className="claim-preview__overlay-properties--small playlist-channel">
+                  <I18nMessage
+                    tokens={{
+                      playlist_channel: (
+                        <UriIndicator
+                          focusable={false}
+                          uri={channel && channel.permanent_url}
+                          link
+                          showHiddenAsAnonymous
+                        >
+                          <ChannelThumbnail uri={channel && channel.permanent_url} xsmall checkMembership={false} />
+                        </UriIndicator>
+                      ),
+                    }}
+                  >
+                    Published as: %playlist_channel%
+                  </I18nMessage>
+                </div>
+              )}
+
               {collectionCount > 0 && (
                 <Button button="alt" label={__('Play All')} icon={ICONS.PLAY} onClick={() => push(firstItem)} />
               )}
             </div>
 
-            {hasClaim ? (
-              <div className="claim-preview__overlay-properties--small playlist-channel">
-                <I18nMessage
-                  tokens={{
-                    playlist_channel: (
-                      <UriIndicator focusable={false} uri={channel && channel.permanent_url} link showHiddenAsAnonymous>
-                        <ChannelThumbnail uri={channel && channel.permanent_url} xsmall checkMembership={false} />
-                      </UriIndicator>
-                    ),
-                  }}
-                >
-                  Published as: %playlist_channel%
-                </I18nMessage>
-              </div>
-            ) : (
-              <div className="claim-preview-info">
-                <CollectionPrivateIcon />
-              </div>
-            )}
+            <div className="claim-preview-info">{hasClaim ? <CollectionPublicIcon /> : <CollectionPrivateIcon />}</div>
 
             <div className="claim-preview-info">
               {__(collectionCount === 1 ? '%playlist_item_count% item' : '%playlist_item_count% items', {
