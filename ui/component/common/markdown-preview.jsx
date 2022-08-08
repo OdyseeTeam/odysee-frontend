38fa5c05662173aee8aa22747ae049ec17fd3d0a
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-08-08 12:16:13 +0200

    Add unicode to emotes constant

--- ui/component/common/markdown-preview.jsx
+++ ui/component/common/markdown-preview.jsx
@@ -17,13 +17,13 @@ import remark from 'remark';
 import remarkAttr from 'remark-attr';
 import remarkBreaks from 'remark-breaks';
 // import remarkTwemoji from 'remark-twemoji';
-const visit = require('unist-util-visit');
-import twemoji from 'twemoji';
+// import twemoji from 'twemoji';
 import remarkEmoji from 'remark-emoji';
 import remarkFrontMatter from 'remark-frontmatter';
 import remarkStrip from 'strip-markdown';
 import ZoomableImage from 'component/zoomableImage';
-
+import { TWEMOTES } from 'constants/emotes';
+const visit = require('unist-util-visit');
 const RE_EMOTE = /:\+1:|:-1:|:[\w-]+:/;
 
 function isEmote(title, src) {
@@ -82,22 +82,45 @@ const remarkTwemoji = (tree) => {
   const EmojiRegExp = new RegExp(
     '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])'
   );
+  /*
   function attributesCallback(rawText) {
     return {
       title: rawText,
     };
   }
+  */
   function transformer(tree) {
     visit(tree, 'text', (node) => {
       if (EmojiRegExp.test(node.value)) {
-        // node.value = ':frog:'
-        const parsedNode = node.value.replace(EmojiRegExp, ':frog:');
-        // node.type = 'emote';
-        node.value = parsedNode.replace(/class/g, 'className');
+        let code = node.value.match(EmojiRegExp)[0];
+
+        const emote = Object.values(TWEMOTES)
+          // $FlowIgnore
+          .map((category) => category.find(({ unicode }) => code === unicode))
+          .filter(function (x) {
+            return x !== undefined;
+          })[0];
+        node.type = 'image';
+        node.url = emote.url;
+        node.title = emote.name;
+        node.children = [{ type: 'text', value: emote.name }];
+        if (!node.data || !node.data.hProperties) {
+          // Create new node data
+          node.data = {
+            hProperties: { emote: true },
+          };
+        } else if (node.data.hProperties) {
+          // Don't overwrite current attributes
+          node.data.hProperties = {
+            emote: true,
+            ...node.data.hProperties,
+          };
+        }
       }
     });
   }
 
+  // return twemoji.parse(transformer)
   return transformer;
 };
 
@@ -303,9 +326,9 @@ export default React.memo<MarkdownProps>(function MarkdownPreview(props: Markdow
           .use(disableTimestamps || isMarkdownPost ? null : inlineTimestamp)
           .use(disableTimestamps || isMarkdownPost ? null : formattedTimestamp)
           // Emojis
-          // .use(remarkTwemoji)
+          .use(remarkTwemoji)
           .use(inlineEmote)
-          .use(formattedEmote)
+          .use(formattedEmote) // :code:
           .use(remarkEmoji)
           // Render new lines without needing spaces.
           .use(remarkBreaks)
