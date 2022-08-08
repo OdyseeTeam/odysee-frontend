01e57a9ad1e9f83b789c1612da7fc4af5765eef7
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-08-08 13:05:26 +0200

    Parse twemojis in page titles

--- ui/component/common/markdown-preview.jsx
+++ ui/component/common/markdown-preview.jsx
@@ -16,8 +16,6 @@ import reactRenderer from 'remark-react';
 import remark from 'remark';
 import remarkAttr from 'remark-attr';
 import remarkBreaks from 'remark-breaks';
-// import remarkTwemoji from 'remark-twemoji';
-// import twemoji from 'twemoji';
 import remarkEmoji from 'remark-emoji';
 import remarkFrontMatter from 'remark-frontmatter';
 import remarkStrip from 'strip-markdown';
@@ -79,20 +77,14 @@ const SimpleText = (props: SimpleTextProps) => {
 };
 
 const remarkTwemoji = (tree) => {
-  const EmojiRegExp = new RegExp(
+  const RE_TWEMOJI = new RegExp(
     '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])'
   );
-  /*
-  function attributesCallback(rawText) {
-    return {
-      title: rawText,
-    };
-  }
-  */
+
   function transformer(tree) {
     visit(tree, 'text', (node) => {
-      if (EmojiRegExp.test(node.value)) {
-        let code = node.value.match(EmojiRegExp)[0];
+      if (RE_TWEMOJI.test(node.value)) {
+        let code = node.value.match(RE_TWEMOJI)[0];
 
         const emote = Object.values(TWEMOTES)
           // $FlowIgnore
@@ -120,7 +112,6 @@ const remarkTwemoji = (tree) => {
     });
   }
 
-  // return twemoji.parse(transformer)
   return transformer;
 };
 
@@ -217,7 +208,6 @@ export default React.memo<MarkdownProps>(function MarkdownPreview(props: Markdow
     isComment,
   } = props;
   let { content } = props;
-  // content = twemoji.parse(content)
 
   const strippedContent = content
     ? content.replace(REPLACE_REGEX, (iframeHtml, iframeUrl) => {
