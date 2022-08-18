aa33de6e482b1b93f6fdb59b05f27d67720f9553
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-08-18 09:23:41 +0200

    Add counter to snackbar

--- ui/page/channel/view.jsx
+++ ui/page/channel/view.jsx
@@ -31,6 +31,7 @@ import PlaceholderTx from 'static/img/placeholderTx.gif';
 import Tooltip from 'component/common/tooltip';
 import { toCompactNotation } from 'util/string';
 import PremiumBadge from 'component/premiumBadge';
+import SnackBar from 'component/snackBar';
 
 export const PAGE_VIEW_QUERY = `view`;
 export const DISCUSSION_PAGE = `discussion`;
@@ -233,6 +234,7 @@ function ChannelPage(props: Props) {
   return (
     <Page className="channelPage-wrapper" noFooter>
       <header className="channel-cover">
+        <SnackBar />
         <div className="channel__quick-actions">
           {isMyYouTubeChannel && (
             <Button
