14789627a9e59d43db927ae3c81a14e67f499199
Rafael <rafael.saes@odysee.com>
2022-09-06 09:20:24 -0300

    feat: also allow the button to be specific

--- ui/component/buttonNavigateChannelId/view.jsx
+++ ui/component/buttonNavigateChannelId/view.jsx
@@ -6,16 +6,20 @@ import { CHANNEL_ID } from 'constants/urlParams';
 type Props = {
   page: string,
   search?: string,
+  channelId?: string,
   // -- redux --
   defaultChannelId: ?string,
   activeChannelId: ?string,
 };
 
 const ButtonNavigateChannelId = (props: Props) => {
-  const { page, search, defaultChannelId, activeChannelId, ...buttonProps } = props;
+  const { page, search, channelId, defaultChannelId, activeChannelId, ...buttonProps } = props;
 
   const urlParams = new URLSearchParams(search);
-  if (defaultChannelId && activeChannelId && defaultChannelId !== activeChannelId) {
+
+  if (channelId) {
+    urlParams.set(CHANNEL_ID, channelId);
+  } else if (defaultChannelId && activeChannelId && defaultChannelId !== activeChannelId) {
     urlParams.set(CHANNEL_ID, activeChannelId);
   }
 
