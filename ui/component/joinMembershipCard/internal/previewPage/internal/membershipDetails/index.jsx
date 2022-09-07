5ab2488a716d8405dd70a75cbd088354018466d2
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-09-07 14:17:54 +0200

    Show membership perks by default

--- ui/component/joinMembershipCard/internal/previewPage/internal/membershipDetails/index.jsx
+++ ui/component/joinMembershipCard/internal/previewPage/internal/membershipDetails/index.jsx
@@ -21,21 +21,25 @@ const MembershipDetails = (props: Props) => {
       <section className="membership-tier__infos">
         <span>{membership.Membership.description}</span>
 
-        {membership.Perks && membership.Perks.length > 0 && (
-          <div className="membership-tier__perks">
-            <div className="membership-tier__moon" />
+        <div className="membership-tier__perks">
+          <div className="membership-tier__moon" />
             <div className="membership-tier__perks-content">
-              <label>{__('Perks')}</label>
-
-              <ul>
-                {/* $FlowFixMe -- already handled above */}
-                {membership.Perks.map((tierPerk, i) => (
-                  <li key={i}>{tierPerk.name}</li>
-                ))}
-              </ul>
-            </div>
+              {membership.Perks && membership.Perks.length > 0 ? (          
+                <>            
+                  <label>{__('Perks')}</label>
+                  <ul>
+                    {/* $FlowFixMe -- already handled above */}
+                    {membership.Perks.map((tierPerk, i) => (
+                      <li key={i}>{tierPerk.name}</li>
+                    ))}
+                  </ul>
+                  
+                </>
+              ) : (
+                <label>{__('No Perks...')}</label>
+              )}
           </div>
-        )}
+        </div>
       </section>
 
       <section className="membership-tier__actions">{headerAction}</section>
