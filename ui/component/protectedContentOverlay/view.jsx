96ffea383c85f94c0f6d6eaebdc8c87062cdb496
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-09-07 13:50:59 +0200

    Fix protectec overlay

--- ui/component/protectedContentOverlay/view.jsx
+++ ui/component/protectedContentOverlay/view.jsx
@@ -13,46 +13,39 @@ type Props = {
 };
 
 export default function ProtectedContentOverlay(props: Props) {
-  const {
-    protectedMembershipIds,
-    activeMembershipIds,
-    claimIsMine,
-    openModal,
-    uri,
-    isProtected,
-  } = props;
+  const { protectedMembershipIds, activeMembershipIds, claimIsMine, openModal, uri, isProtected } = props;
 
   const [userIsAMember, setUserIsAMember] = React.useState(false);
 
   React.useEffect(() => {
     if (protectedMembershipIds && activeMembershipIds && isProtected) {
-
-      setUserIsAMember(activeMembershipIds.some(id => protectedMembershipIds.includes(id)));
+      setUserIsAMember(activeMembershipIds.some((id) => protectedMembershipIds.includes(id)));
     }
   }, [protectedMembershipIds, activeMembershipIds, isProtected]);
 
   // don't show overlay if it's not protected or user is a member
-  if (!isProtected || userIsAMember || claimIsMine) return (<></>);
+  if (!isProtected || userIsAMember || claimIsMine) return <></>;
 
   return (
     <>
       <div className="protected-content-overlay">
-        <div style={{ textAlign: 'center', marginTop: '19px' }}>
-          <Icon icon={ICONS.LOCK} className="protected-content-lock" />
+        <div>
+          <Icon icon={ICONS.LOCK} />
+          <span>Only channel members can view this content</span>
+          <Button
+            button="primary"
+            icon={ICONS.UPGRADE}
+            label={__('Membership Options')}
+            title={__('Become A Member')}
+            onClick={() =>
+              openModal(MODALS.JOIN_MEMBERSHIP, {
+                uri,
+                protectedMembershipIds,
+              })
+            }
+            // style={{ filter: !creatorHasMemberships ? 'brightness(50%)' : undefined }}
+          />
         </div>
-        <h1>Only channel members can view this content</h1>
-        <Button
-          className="protected-content-overlay__button"
-          button="primary"
-          icon={ICONS.UPGRADE}
-          label={__('See Membership Options')}
-          title={__('Become A Member')}
-          onClick={() => openModal(MODALS.JOIN_MEMBERSHIP, {
-            uri,
-            protectedMembershipIds,
-          })}
-          // style={{ filter: !creatorHasMemberships ? 'brightness(50%)' : undefined }}
-        />
       </div>
     </>
   );
