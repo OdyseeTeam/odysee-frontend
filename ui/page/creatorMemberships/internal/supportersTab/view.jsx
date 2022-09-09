cf54c11113809d0f3a6735ba2b5972fb57ab2392
Anthony <contact@anthonymayfield.com>
2022-09-09 13:47:15 +0200

    add supporters tab

--- ui/page/creatorMemberships/internal/supportersTab/view.jsx
+++ ui/page/creatorMemberships/internal/supportersTab/view.jsx
@@ -1,50 +1,69 @@
 // @flow
 import React from 'react';
 import ChannelThumbnail from 'component/channelThumbnail';
+import { Lbryio } from 'lbryinc';
+import moment from 'moment';
 
-export default function SupportersTab() {
-  // TODO: replace with API call
-  const yourSupporters = [
-    {
-      channelName: '@test35234',
-      tierName: 'Community MVP',
-      supportAmountPerMonth: '20',
-      currency: 'USD',
-      monthsOfSupport: 2,
-    },
-  ];
+import { getStripeEnvironment } from 'util/stripe';
+const stripeEnvironment = getStripeEnvironment();
+
+type Props = {
+  // -- redux --
+  activeChannelClaim: ?ChannelClaim,
+  bankAccountConfirmed: ?boolean,
+  doTipAccountStatus: (any) => void,
+};
+
+const SupportersTab = (props: Props) => {
+  const { activeChannelClaim } = props;
+
+  const [supportersResponse, setSupportersResponse] = React.useState();
+
+  React.useEffect(() => {
+    (async function() {
+      const response = await Lbryio.call('membership', 'supporters_list', {
+        environment: stripeEnvironment,
+      });
+
+      l(response);
+      setSupportersResponse(response)
+
+    })();
+  }, [activeChannelClaim]);
+
+  l(activeChannelClaim);
 
   return (
     <div className="membership-table__wrapper">
       <table className="table">
         <thead>
           <tr>
-            <th className="date-header">Channel Name</th>
-            <th className="channelName-header">Tier</th>
-            <th className="location-header">Amount</th>
-            <th className="amount-header">Joined On</th>
-            <th className="channelName-header">Membership Length</th>
+            <th className="date-header">{__('Channel Name')}</th>
+            <th className="channelName-header">{__('Tier')}</th>
+            <th className="location-header">{__('Amount')}</th>
+            <th className="amount-header">{__('Joined On')}</th>
+            <th className="channelName-header">{__('Months Supporting')}</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {yourSupporters.map((supporter, i) => (
-              <>
+            {supportersResponse && supportersResponse.map((supporter, i) => (
+              <tr>
                 <td>
                   <span dir="auto" className="button__label">
                     {false && <ChannelThumbnail xsmall uri={channelClaim.canonical_url} />}
-                    @test35234
+                    {supporter.ChannelName}
                   </span>
                 </td>
-                <td>Community MVP</td>
-                <td>$20 USD / Month</td>
-                <td>March 19, 2022</td>
-                <td>2 Months</td>
-              </>
+                <td>{supporter.MembershipName}</td>
+                <td>${supporter.Price / 100} USD / Month</td>
+                <td>{moment(new Date(supporter.JoinedAtTime)).format('MMMM Do YYYY')}</td>
+                <td>{Math.ceil(moment(new Date()).diff(new Date(supporter.JoinedAtTime), 'months', true))}</td>
+              </tr>
             ))}
-          </tr>
         </tbody>
       </table>
     </div>
   );
-}
+};
+
+export default SupportersTab;
