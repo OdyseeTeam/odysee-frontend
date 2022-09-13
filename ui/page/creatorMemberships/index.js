7318b1607538e410edc5de123e17b507ace654f8
Anthony <contact@anthonymayfield.com>
2022-09-13 14:12:46 +0200

    touchup

--- ui/page/creatorMemberships/index.js
+++ ui/page/creatorMemberships/index.js
@@ -4,12 +4,19 @@ import { selectActiveChannelClaim } from 'redux/selectors/app';
 import { selectAccountChargesEnabled } from 'redux/selectors/stripe';
 import { doTipAccountStatus } from 'redux/actions/stripe';
 import { selectMyChannelClaims } from 'redux/selectors/claims';
+import { selectMyMembershipTiers } from 'redux/selectors/memberships';
 
-const select = (state) => ({
-  activeChannelClaim: selectActiveChannelClaim(state),
-  bankAccountConfirmed: selectAccountChargesEnabled(state),
-  myChannelClaims: selectMyChannelClaims(state),
-});
+const select = (state, props) => {
+  const activeChannel = selectActiveChannelClaim(state);
+  const myMembershipTiers = selectMyMembershipTiers(state, activeChannel?.claim_id);
+
+  return {
+    hasTiers: myMembershipTiers?.length,
+    bankAccountConfirmed: selectAccountChargesEnabled(state),
+    activeChannelClaim: selectActiveChannelClaim(state),
+    myChannelClaims: selectMyChannelClaims(state),
+  };
+};
 
 const perform = {
   doTipAccountStatus,
