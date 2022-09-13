4bf4afe78cf9a269cbcc2cccc4c91f4734d1bc1b
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-09-13 10:33:05 +0200

    Update some logic & design

--- ui/page/creatorMemberships/internal/overviewTab/view.jsx
+++ ui/page/creatorMemberships/internal/overviewTab/view.jsx
@@ -8,6 +8,7 @@ import Button from 'component/button';
 import CopyableText from 'component/copyableText';
 import ChannelThumbnail from 'component/channelThumbnail';
 import ButtonNavigateChannelId from 'component/buttonNavigateChannelId';
+import './style.scss';
 
 type Props = {
   channelsToList: ?Array<ChannelClaim>,
@@ -33,71 +34,71 @@ function OverviewTab(props: Props) {
           <div className="membership__mychannels-header">
             <label>My Earnings</label>
           </div>
-          <div className="membership-table__wrapper">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th className="channelName-header">Channel Name</th>
-                  <th>{__('Supporters')}</th>
-                  <th>{__('Estimated Monthly Income')}</th>
-                  <th>{__('Total Received')}</th>
-                  <th className="membership-table__page">{__('Page')}</th>
-                  <th className="membership-table__url">{__('URL')}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {channelsToList.map((channelClaim) => (
-                  <tr key={channelClaim.claim_id} onClick={() => selectChannel(channelClaim)}>
-                    <td>
-                      <ChannelThumbnail xsmall uri={channelClaim.canonical_url} />
-                      {channelClaim.value.title || channelClaim.name}
-                    </td>
-                    <td>0</td>
-                    <td>$0</td>
-                    <td>$0</td>
-                    <td>
-                      <ButtonNavigateChannelId
-                        button="primary"
-                        // className="membership_button"
-                        // label={__('View your membership page')}
-                        icon={ICONS.UPGRADE}
-                        navigate={`${formatLbryUrlForWeb(channelClaim.canonical_url)}?view=membership`}
-                      />
-                    </td>
-                    <td className="membership-table__url">
-                      <CopyableText
-                        onlyCopy
-                        primaryButton
-                        copyable={`${URL}${formatLbryUrlForWeb(channelClaim.canonical_url)}?view=membership`}
-                        snackMessage={__('Page location copied')}
-                      />
-                    </td>
+          {bankAccountConfirmed && (
+            <div className="membership-table__wrapper">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th className="channelName-header">Channel Name</th>
+                    <th>{__('Supporters')}</th>
+                    <th>{__('Estimated Monthly Income')}</th>
+                    <th>{__('Total Received')}</th>
+                    <th className="membership-table__page">{__('Page')}</th>
+                    <th className="membership-table__url">{__('URL')}</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </thead>
+                <tbody>
+                  {channelsToList.map((channelClaim) => (
+                    <tr key={channelClaim.claim_id} onClick={() => selectChannel(channelClaim)}>
+                      <td>
+                        <ChannelThumbnail xsmall uri={channelClaim.canonical_url} />
+                        {channelClaim.value.title || channelClaim.name}
+                      </td>
+                      <td>0</td>
+                      <td>$0</td>
+                      <td>$0</td>
+                      <td>
+                        <ButtonNavigateChannelId
+                          button="primary"
+                          // className="membership_button"
+                          // label={__('View your membership page')}
+                          icon={ICONS.UPGRADE}
+                          navigate={`${formatLbryUrlForWeb(channelClaim.canonical_url)}?view=membership`}
+                        />
+                      </td>
+                      <td className="membership-table__url">
+                        <CopyableText
+                          onlyCopy
+                          primaryButton
+                          copyable={`${URL}${formatLbryUrlForWeb(channelClaim.canonical_url)}?view=membership`}
+                          snackMessage={__('Page location copied')}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </>
       )}
 
       {/* Bank account connection status */}
-      <div className="bank-account-information__div">
-        <div className="bank-account-status__div">
-          {!bankAccountConfirmed && (
-            <>
-              <h1>{__('Bank Account Status')}</h1>
-              <h2>{__('To be able to begin receiving payments you must connect a Bank Account first')}</h2>
-              <Button
-                button="primary"
-                className="membership_button"
-                label={__('Connect a bank account')}
-                icon={ICONS.FINANCE}
-                navigate={`$/${PAGES.SETTINGS_STRIPE_ACCOUNT}`}
-                style={{ maxWidth: '254px' }}
-              />
-            </>
-          )}
-        </div>
+      <div className="bank-account-status">
+        {!bankAccountConfirmed && (
+          <>
+            <div className="">
+              <label>{__('Bank Account Status')}</label>
+              <span>{__('To be able to begin receiving payments you must connect a Bank Account first.')}</span>
+            </div>
+            <Button
+              button="primary"
+              label={__('Connect a bank account')}
+              icon={ICONS.FINANCE}
+              navigate={`$/${PAGES.SETTINGS_STRIPE_ACCOUNT}`}
+            />
+          </>
+        )}
       </div>
     </>
   );
