21f081cfa3d870987244fb6095dbe17204433bd2
Rafael <rafael.saes@odysee.com>
2022-09-14 10:32:15 -0300

    fix: punctuation

--- ui/redux/actions/memberships.js
+++ ui/redux/actions/memberships.js
@@ -198,7 +198,7 @@ export const doOpenCancelationModalForMembership = (membership: MembershipTier) 
       subtitle: __(
         'Are you sure you want to cancel your %creator_name%\'s "%membership_name%" membership? ' +
           'You will still have all your features until %end_date% at which point your purchase will not be renewed ' +
-          'and you will lose access to your membership features and perks',
+          'and you will lose access to your membership features and perks.',
         {
           creator_name: creatorTitleName,
           membership_name: MembershipDetails.name,
