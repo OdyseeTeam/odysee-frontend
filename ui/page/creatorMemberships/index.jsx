315c94b53c8dfe5d377d9caa85114a3686ddceb3
Rafael Saes <rafael.saes@odysee.com>
2022-09-29 15:54:25 -0300

    feat: push to page onClick for portal

--- ui/page/creatorMemberships/index.jsx
+++ ui/page/creatorMemberships/index.jsx
@@ -9,41 +9,51 @@ import Page from 'component/page';
 import HelpHub from 'component/common/help-hub';
 
 import './style.scss';
+import { useHistory } from 'react-router-dom';
 
-const MembershipsLandingPage = () => (
-  <Page className="premium-wrapper">
-    <div>
-      <h1 className="membership-wrapper-title">
-        <Icon icon={ICONS.MEMBERSHIP} size={10} />
-        <label>{__('Memberships')}</label>
-      </h1>
-    </div>
+const MembershipsLandingPage = () => {
+  const { push } = useHistory();
 
-    <div className="membership-wrapper supporter">
-      <div className="membership-content">
-        <div>
-          <h2>{__('Donor Portal')}</h2>
-          <p className="portal-tagline">{__('Find creators you like and support them.')}</p>
-          <Button button="primary" navigate={`/$/${PAGES.MEMBERSHIPS_SUPPORTER}`} label={__('Enter Donor Portal')} />
+  function handleNavigateToPage(page: string) {
+    push(page);
+  }
+  return (
+    <Page className="premium-wrapper">
+      <div>
+        <h1 className="membership-wrapper-title">
+          <Icon icon={ICONS.MEMBERSHIP} size={10} />
+          <label>{__('Memberships')}</label>
+        </h1>
+      </div>
+
+      <div className="membership-wrapper supporter" onClick={() => handleNavigateToPage(PAGES.MEMBERSHIPS_SUPPORTER)}>
+        <div className="membership-content">
+          <div>
+            <h2>{__('Donor Portal')}</h2>
+            <p className="portal-tagline">{__('Find creators you like and support them.')}</p>
+            <Button button="primary" navigate={`/$/${PAGES.MEMBERSHIPS_SUPPORTER}`} label={__('Enter Donor Portal')} />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="membership-wrapper memberships">
-      <div className="membership-content">
-        <div>
-          <h2>{__('Membership Portal')}</h2>
-          <p className="portal-tagline">{__('Create memberships and have users subscribe to them to support you.')}</p>
-          <Button button="primary" navigate={`/$/${PAGES.CREATOR_MEMBERSHIPS}`} label={__('Manage Memberships')} />
+      <div className="membership-wrapper memberships" onClick={() => handleNavigateToPage(PAGES.CREATOR_MEMBERSHIPS)}>
+        <div className="membership-content">
+          <div>
+            <h2>{__('Membership Portal')}</h2>
+            <p className="portal-tagline">
+              {__('Create memberships and have users subscribe to them to support you.')}
+            </p>
+            <Button button="primary" navigate={`/$/${PAGES.CREATOR_MEMBERSHIPS}`} label={__('Manage Memberships')} />
+          </div>
         </div>
       </div>
-    </div>
-    <HelpHub
-      href="https://help.odysee.tv/category-memberships/"
-      image="LadyFungus"
-      text={__('What are Memberships? Lady Fungus can explain it in the %help_hub%.')}
-    />
-  </Page>
-);
+      <HelpHub
+        href="https://help.odysee.tv/category-memberships/"
+        image="LadyFungus"
+        text={__('What are Memberships? Lady Fungus can explain it in the %help_hub%.')}
+      />
+    </Page>
+  );
+};
 
 export default MembershipsLandingPage;
