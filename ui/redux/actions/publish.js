5cc368370fb2b71fcd1415abc10811110eb1a4a4
Anthony <contact@anthonymayfield.com>
2022-10-06 19:37:22 +0200

    bugfix prepare edit

--- ui/redux/actions/publish.js
+++ ui/redux/actions/publish.js
@@ -645,7 +645,7 @@ export const doPrepareEdit = (claim: StreamClaim, uri: string, claimType: string
         protectedMembershipIds = selectProtectedContentMembershipsForClaimId(state, channelId, claim.claim_id);
       }
 
-      publishData['restrictedToMemberships'] = protectedMembershipIds.join(',');
+      publishData['restrictedToMemberships'] = protectedMembershipIds?.join(',');
     } else {
       publishData.tags = publishData.tags
         ? publishData.tags.filter((tag) => tag.name === MEMBERS_ONLY_CONTENT_TAG)
