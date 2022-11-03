cc3e1fa49b2575429048086dfc2d08e4201e3a08
Rafael Saes <rafael.saes@odysee.com>
2022-11-03 11:26:37 -0300

    fix: flow lint

--- ui/component/embedTextArea/view.jsx
+++ ui/component/embedTextArea/view.jsx
@@ -14,7 +14,7 @@ type Props = {
   includeStartTime: boolean,
   startTime: number,
   referralCode: ?string,
-  newestType?: boolean,
+  newestType?: string,
 };
 
 export default function EmbedTextArea(props: Props) {
