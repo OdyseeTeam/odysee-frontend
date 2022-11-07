6dc29ae4ecbbda755eee22eb1306701c67c4b61a
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-11-07 13:27:06 +0100

    Fix portal page

--- ui/page/portal/view.jsx
+++ ui/page/portal/view.jsx
@@ -28,31 +28,33 @@ function PortalPage(props: Props) {
       // theme[0].style.backgroundImage = 'linear-gradient(312deg, rgba(0,0,0,1) 0%, rgba(101,15,124,1) 100%)';
       // theme[0].style.backgroundImage = 'linear-gradient(312deg, rgba(0,0,0,1) 40%, rgba(101,15,124,1) 100%)';
       // theme[0].style.backgroundImage = 'radial-gradient(circle at 80% 20%, #140019, #000 50%, rgba(200,200,200,0.2) 25%, rgba(101,15,124,0.9) 75%)'
-      theme[0].style.backgroundImage =
-        'radial-gradient(circle at 80% 20%, rgba(0,0,0,0.6), #000 50%, rgba(101,15,124,0.9) 25%, #000 75%)';
-      theme[0].classList.add('theme-stars');
+      if (theme && theme.length) {
+        theme[0].style.backgroundImage =
+          'radial-gradient(circle at 80% 20%, rgba(0,0,0,0.6), #000 50%, rgba(101,15,124,0.9) 25%, #000 75%)';
+        theme[0].classList.add('theme-stars');
+      }
     }
   }, [portals]);
 
   if (portal) console.log('portal: ', portal);
 
-  return (
-    portal && (
-      <>
-        <Page className="portal-wrapper" fullWidthPage>
-          <div className="portal-header">
-            <img src={portal.image} />
-            <div className="portal-meta">
-              <h1>{portal.label}</h1>
-              <p>{portal.description}</p>
-            </div>
+  return portal ? (
+    <>
+      <Page className="portal-wrapper" fullWidthPage>
+        <div className="portal-header">
+          <img src={portal.image} />
+          <div className="portal-meta">
+            <h1>{portal.label}</h1>
+            <p>{portal.description}</p>
           </div>
-          <div className="portal-content">
-            <ClaimTilesDiscover claimIds={(portal.claimIds && portal.claimIds.videos) || []} uris={[]} pageSize={18} />
-          </div>
-        </Page>
-      </>
-    )
+        </div>
+        <div className="portal-content">
+          <ClaimTilesDiscover claimIds={(portal.claimIds && portal.claimIds.videos) || []} uris={[]} pageSize={18} />
+        </div>
+      </Page>
+    </>
+  ) : (
+    <></>
   );
 }
 
