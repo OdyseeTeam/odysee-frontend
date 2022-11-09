d588280ce575f7c5cabcff71bd1288e434f709da
toshokanneko <raphael.wickihalder@odysee.com>
2022-11-09 13:22:40 +0100

    Make portals killable

--- ui/component/portals/view.jsx
+++ ui/component/portals/view.jsx
@@ -1,21 +1,49 @@
 // @flow
 import React from 'react';
+import Icon from 'component/common/icon';
+import * as ICONS from 'constants/icons';
+import * as SETTINGS from 'constants/settings';
+import classnames from 'classnames';
 import { NavLink } from 'react-router-dom';
 import './style.scss';
+
+type HomepageOrder = { active: ?Array<string>, hidden: ?Array<string> };
+
 type Props = {
   portals: any,
+  homepageOrder: HomepageOrder,
+  // --- perform ---
+  doSetClientSetting: (key: string, value: any, push: boolean) => void,
 };
 
 export default function Portals(props: Props) {
-  const { portals } = props;
+  const { portals, homepageOrder, doSetClientSetting } = props;
+  const [kill, setKill] = React.useState(false);
 
   if (portals && portals.mainPortal) {
     portals.mainPortal.portals = portals.mainPortal.portals.concat(portals.mainPortal.portals);
     portals.mainPortal.portals = portals.mainPortal.portals.splice(0, 6);
   }
 
+  function removePortals() {
+    let orderToSave = homepageOrder;
+    if (orderToSave.active && orderToSave.active.includes('PORTALS')) {
+      orderToSave.active.splice(orderToSave.active.indexOf('PORTALS'), 1);
+      if (orderToSave.hidden) {
+        orderToSave.hidden.push('PORTALS');
+      } else {
+        orderToSave.hidden = ['PORTALS'];
+      }
+      doSetClientSetting(SETTINGS.HOMEPAGE_ORDER, orderToSave, true);
+      setKill(true);
+    }
+  }
+
   return portals && portals.mainPortal ? (
-    <div className="portals-wrapper" style={{ backgroundImage: 'url(' + portals.mainPortal.background + ')' }}>
+    <div
+      className={classnames('portals-wrapper', { kill: kill })}
+      style={{ backgroundImage: 'url(' + portals.mainPortal.background + ')' }}
+    >
       <h1>{portals.mainPortal.description}</h1>
       {portals.mainPortal.portals.map((portal, i) => {
         return (
@@ -31,14 +59,19 @@ export default function Portals(props: Props) {
           </div>
         );
       })}
-      <div className="portal-browse left">‹</div>
-      <div className="portal-browse right">›</div>
+      {/*
+        <div className="portal-browse left">‹</div>
+        <div className="portal-browse right">›</div>
 
-      <div className="portal-page-indicator">
-        <div />
-        <div />
-        <div />
-        <div />
+        <div className="portal-page-indicator">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      */}
+      <div className="portals-remove" onClick={() => removePortals()}>
+        <Icon icon={ICONS.REMOVE} />
       </div>
     </div>
   ) : (
