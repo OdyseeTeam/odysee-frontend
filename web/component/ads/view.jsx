8ca7d956afc8cc0d5087338a018712b9e0766899
toshokanneko <raphael.wickihalder@odysee.com>
2022-12-06 15:13:12 +0100

    Update tile ad

--- web/component/ads/view.jsx
+++ web/component/ads/view.jsx
@@ -1,13 +1,14 @@
 // @flow
-import * as PAGES from 'constants/pages';
+// import * as PAGES from 'constants/pages';
 import React from 'react';
-import I18nMessage from 'component/i18nMessage';
-import Button from 'component/button';
+// import I18nMessage from 'component/i18nMessage';
+// import Button from 'component/button';
 import PremiumPlusTile from 'component/premiumPlusTile';
-import classnames from 'classnames';
-import Icon from 'component/common/icon';
-import * as ICONS from 'constants/icons';
-import { useIsMobile } from 'effects/use-screensize';
+// import classnames from 'classnames';
+// import Icon from 'component/common/icon';
+// import * as ICONS from 'constants/icons';
+// import { useIsMobile } from 'effects/use-screensize';
+import './style.scss';
 
 const DISABLE_VIDEO_AD = false;
 
@@ -16,6 +17,9 @@ const AD_CONFIGS = Object.freeze({
   ADNIMATION: {
     url: 'https://tg1.aniview.com/api/adserver/spt?AV_TAGID=6252bb6f28951333ec10a7a6&AV_PUBLISHERID=601d9a7f2e688a79e17c1265',
     tag: 'AV6252bb6f28951333ec10a7a6',
+  },
+  REVCONTENT: {
+    url: 'https://assets.revcontent.com/master/delivery.js',
   },
 });
 
@@ -34,9 +38,10 @@ type Props = {
 };
 
 function Ads(props: Props) {
-  const { type = 'video', tileLayout, small, shouldShowAds, className, noFallback } = props;
-  const isMobile = useIsMobile();
-  const adConfig = AD_CONFIGS.ADNIMATION;
+  const { type = 'video', tileLayout, shouldShowAds, noFallback } = props;
+  // const isMobile = useIsMobile();
+  // const adConfig = AD_CONFIGS.ADNIMATION;
+  const adConfig = AD_CONFIGS.REVCONTENT;
 
   React.useEffect(() => {
     if (shouldShowAds && !DISABLE_VIDEO_AD) {
@@ -55,6 +60,7 @@ function Ads(props: Props) {
     }
   }, [shouldShowAds, adConfig]);
 
+  /*
   const adsSignInDriver = !isMobile ? (
     <I18nMessage
       tokens={{
@@ -68,9 +74,21 @@ function Ads(props: Props) {
   ) : (
     <Button button="link" label={__('Get Odysee Premium+')} navigate={`/$/${PAGES.ODYSEE_MEMBERSHIP}`} />
   );
+  */
 
   if (type === 'video') {
     if (shouldShowAds) {
+      return (
+        <div
+          className="rc_tile"
+          id="rc-widget-fceddd"
+          data-rc-widget
+          data-widget-host="habitat"
+          data-endpoint="//trends.revcontent.com"
+          data-widget-id="273434"
+        />
+      );
+      /*
       return (
         <div
           className={classnames('ads ads__claim-item', className, {
@@ -97,6 +115,7 @@ function Ads(props: Props) {
           </div>
         </div>
       );
+      */
     } else if (!noFallback) {
       return <PremiumPlusTile tileLayout={tileLayout} />;
     }
