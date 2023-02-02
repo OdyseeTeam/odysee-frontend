afabd1f48a5590f8b7c124bd214723314fc571ce
toshokanneko <raphael.wickihalder@odysee.com>
2023-02-02 17:28:06 +0100

    Revert notification uri change

--- ui/component/notification/helpers/target.jsx
+++ ui/component/notification/helpers/target.jsx
@@ -59,6 +59,7 @@ function getUrlParams(notification, notificationTarget) {
 export function getNotificationLink(notification: WebNotification, target: ?string) {
   const notificationTarget = target || getNotificationTarget(notification);
   const urlParams = getUrlParams(notification, notificationTarget).toString();
+  /*
   const prefix = formatLbryUrlForWeb(notification?.notification_parameters?.dynamic?.channel_url);
   const channelUri = prefix.substr(0, prefix.indexOf(':') + 4);
   const canonicalUri =
@@ -67,7 +68,8 @@ export function getNotificationLink(notification: WebNotification, target: ?stri
       : formatLbryUrlForWeb(notificationTarget);
 
   return `${canonicalUri}${urlParams ? `?${urlParams}` : ''}`;
-  // return `${formatLbryUrlForWeb(notificationTarget)}${urlParams ? `?${urlParams}` : ''}`;
+  */
+  return `${formatLbryUrlForWeb(notificationTarget)}${urlParams ? `?${urlParams}` : ''}`;
 }
 
 export function getNotificationLocation(notification: WebNotification, target: ?string) {
