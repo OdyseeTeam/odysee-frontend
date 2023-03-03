40b6cf5812f5bdb94f261197560574910111a471
infinite-persistence <inf.persistence@gmail.com>
2023-03-04 00:17:47 +0800

    Utilize "?hp=xx" in Odysee
    Previously, doFetchHomepages() performs 2 things:
    - load from built-in
    - fetch and update with latest one (in case the built-in is from browser cache)
    
    Now, split this up to 2 separate functions:
    - doLoadBuiltInHomepageData: called during startup
    - doFetchHomepages: called at least once per startup, and on subsequent homepage-selection changes.

--- ui/util/homepages.js
+++ ui/util/homepages.js
@@ -1,6 +1,28 @@
 // @flow
 
 /**
+ * Merges data, either partially or completely, from newDb to oldDb.
+ *
+ * @param oldDb
+ * @param newDb
+ * @param hp If provided, only update the data for this specified homepage.
+ * @returns The new/merged db.
+ */
+export function updateHomepageDb(oldDb: ?HomepagesDb, newDb: ?HomepagesDb, hp: ?string) {
+  if (!oldDb && newDb) {
+    return newDb;
+  } else if (!oldDb || !newDb) {
+    return oldDb;
+  }
+
+  if (hp) {
+    return { ...oldDb, [hp]: newDb[hp] };
+  } else {
+    return { ...newDb };
+  }
+}
+
+/**
  * Hook to post-process or customize the fetched homepage data.
  *
  * The only change for now is to point empty `portals` and `featured` to the
