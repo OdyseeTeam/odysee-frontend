ec6db63c54fa490ee201c5e2fab3ded112d5f82f
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2023-03-22 13:54:24 +0100

    Update claim placeholders

--- ui/component/common/claim-preview-loading.jsx
+++ ui/component/common/claim-preview-loading.jsx
@@ -15,7 +15,7 @@ function ClaimPreviewLoading(props: Props) {
   return (
     // Uses the same WrapperElement as claimPreview so it's consistent with the rest of the list components
     <WrapperElement
-      className={classnames('placeholder claim-preview__wrapper', {
+      className={classnames('placeholder claim-preview__wrapper claim-preview__wrapper--row', {
         'claim-preview__wrapper--channel': isChannel && type !== 'inline',
         'claim-preview__wrapper--inline': type === 'inline',
         'claim-preview__wrapper--recommendation': type === 'small',
