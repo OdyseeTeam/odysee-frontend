15eddc92682d05d48756f688685c378921d3bb8b
infinite-persistence <inf.persistence@gmail.com>
2023-04-14 16:35:48 +0800

    Change env to ensure asserts are off in Production
    Asserts are in debug or IS_TEST_INSTANCE=true
    Can forcefully disable it with FORCE_NO_ASSERT=true

--- ui/asserts.js
+++ ui/asserts.js
@@ -1,14 +1,22 @@
 // @flow
 
-/**
- * 1. [ASSERTS_ENABLED] env enables or disables it on compile-time.
- * 2. [window.app.pause_asserts] is the runtime control (e.g. to temporarily disable it).
- *
- * @param condition
- * @param msg
- */
 export function assert(condition: any, msg: string = 'Assertion failed') {
-  // @if ASSERTS_ENABLED='true'
+  // @if FORCE_NO_ASSERTS='true'
+  if (true) return; // eslint-disable-line no-constant-condition
+  // @endif
+
+  // Need to upgrade our preprocessor, currently doesn't have OR.
+
+  // @if NODE_ENV!='production'
+  doAssert(condition, msg);
+  // @endif
+
+  // @if IS_TEST_INSTANCE='true'
+  doAssert(condition, msg);
+  // @endif
+}
+
+function doAssert(condition: any, msg: string) {
   if (!condition && !window.app.pause_asserts) {
     const error = new Error(msg);
 
@@ -23,5 +31,4 @@ export function assert(condition: any, msg: string = 'Assertion failed') {
 
     console.error(error); // eslint-disable-line no-console
   }
-  // @endif
 }
