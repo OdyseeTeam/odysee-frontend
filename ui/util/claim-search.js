d0ccb1f406f8b46c17039b67af1391dfddbbe054
infinite-persistence <inf.persistence@gmail.com>
2023-04-26 17:44:14 +0800

    Rename: CsOptions -> CsOptHelper
    There is already the SDK's ClaimSearchOptions type, so that's confusing.
    
    The new name will also be a bit clearer, indicating it's a generator.

--- ui/util/claim-search.js
+++ ui/util/claim-search.js
@@ -5,9 +5,9 @@ import * as CS from 'constants/claim_search';
 import { MATURE_TAGS, MEMBERS_ONLY_CONTENT_TAG } from 'constants/tags';
 
 /**
- * Helper functions to derive the ClaimSearch option payload.
+ * Common logic to generate ClaimSearch option payload.
  */
-export const CsOptions = {
+export const CsOptHelper = {
   not_tags: (input?: NotTagInput = {}) => {
     const not_tags = input.showNsfw ? [] : MATURE_TAGS.slice();
 
