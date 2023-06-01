4daad2feef1b30b84a2b04c2af1d16ab6b185cc4
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2023-06-01 09:08:11 +0200

    Revert tab spacing

--- ui/component/claimListDiscover/index.js
+++ ui/component/claimListDiscover/index.js
@@ -22,18 +22,18 @@ function resolveHideMembersOnly(global, override) {
 
 // prettier-ignore
 const select = (state, props) => ({
-    followedTags: selectFollowedTags(state),
-    claimSearchByQuery: selectClaimSearchByQuery(state),
-    claimSearchByQueryLastPageReached: selectClaimSearchByQueryLastPageReached(state),
-    claimsByUri: selectClaimsByUri(state),
-    claimsById: selectById(state),
-    loading: props.loading !== undefined ? props.loading : selectFetchingClaimSearch(state),
-    showNsfw: selectShowMatureContent(state),
-    hideMembersOnly: resolveHideMembersOnly(selectClientSetting(state, SETTINGS.HIDE_MEMBERS_ONLY_CONTENT), props.hideMembersOnly),
-    hideReposts: selectClientSetting(state, SETTINGS.HIDE_REPOSTS),
-    languageSetting: selectLanguage(state),
-    searchInLanguage: selectClientSetting(state, SETTINGS.SEARCH_IN_LANGUAGE),
-    mutedAndBlockedChannelIds: selectMutedAndBlockedChannelIds(state),
+  followedTags: selectFollowedTags(state),
+  claimSearchByQuery: selectClaimSearchByQuery(state),
+  claimSearchByQueryLastPageReached: selectClaimSearchByQueryLastPageReached(state),
+  claimsByUri: selectClaimsByUri(state),
+  claimsById: selectById(state),
+  loading: props.loading !== undefined ? props.loading : selectFetchingClaimSearch(state),
+  showNsfw: selectShowMatureContent(state),
+  hideMembersOnly: resolveHideMembersOnly(selectClientSetting(state, SETTINGS.HIDE_MEMBERS_ONLY_CONTENT), props.hideMembersOnly),
+  hideReposts: selectClientSetting(state, SETTINGS.HIDE_REPOSTS),
+  languageSetting: selectLanguage(state),
+  searchInLanguage: selectClientSetting(state, SETTINGS.SEARCH_IN_LANGUAGE),
+  mutedAndBlockedChannelIds: selectMutedAndBlockedChannelIds(state),
 });
 
 const perform = {
