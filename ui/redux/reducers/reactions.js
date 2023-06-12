aabdc3e63cd68b6070cbdec38dd379b478d4d86f
infinite-persistence <inf.persistence@gmail.com>
2023-06-12 16:14:31 +0800

    Use immer in Reactions slice.
    ## Why
    Discussed with team and decided to use immer moving forward to solve the following:
    - Easier to write immutable code (no more spreading everywhere)
    - If the reducer involves conditionals and ended up not changing the value, the original reference will be returned to save up one render.
    
    ## Change
    Using a non-critical functionality (file reactions) as a trial-run.
    
    Browser-support for `Proxy` is just slightly less than `Promise.all` that we've been using, so it shouldn't impact any of our existing users, but wanted to be sure.

--- ui/redux/reducers/reactions.js
+++ ui/redux/reducers/reactions.js
@@ -1,4 +1,5 @@
 // @flow
+import { produce } from 'immer';
 import { handleActions } from 'util/redux-utils';
 import * as ACTIONS from 'constants/action_types';
 import * as REACTION_TYPES from 'constants/reactions';
@@ -19,36 +20,28 @@ export default handleActions(
     [ACTIONS.REACTIONS_LIST_COMPLETED]: (state, action) => {
       const { claimId, reactions } = action.data;
 
-      const reactionsById = { ...state.reactionsById, [claimId]: reactions };
-      return {
-        ...state,
-        fetchingreactions: false,
-        reactionsById,
-      };
+      return produce(state, (draft) => {
+        draft.reactionsById[claimId] = reactions;
+        draft.fetchingReactions = false;
+      });
     },
     [ACTIONS.REACTIONS_LIKE_COMPLETED]: (state, action) => {
       const { claimId, shouldRemove } = action.data;
-      const reactionsById = { ...state.reactionsById };
-      reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.LIKE] = shouldRemove ? 0 : 1;
-      reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.DISLIKE] = 0;
 
-      return {
-        ...state,
-        fetchingreactions: false,
-        reactionsById,
-      };
+      return produce(state, (draft) => {
+        draft.reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.LIKE] = shouldRemove ? 0 : 1;
+        draft.reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.DISLIKE] = 0;
+        draft.fetchingReactions = false;
+      });
     },
     [ACTIONS.REACTIONS_DISLIKE_COMPLETED]: (state, action) => {
       const { claimId, shouldRemove } = action.data;
-      const reactionsById = { ...state.reactionsById };
-      reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.DISLIKE] = shouldRemove ? 0 : 1;
-      reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.LIKE] = 0;
 
-      return {
-        ...state,
-        fetchingreactions: false,
-        reactionsById,
-      };
+      return produce(state, (draft) => {
+        draft.reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.DISLIKE] = shouldRemove ? 0 : 1;
+        draft.reactionsById[claimId].my_reactions[claimId][REACTION_TYPES.LIKE] = 0;
+        draft.fetchingReactions = false;
+      });
     },
   },
   defaultState
