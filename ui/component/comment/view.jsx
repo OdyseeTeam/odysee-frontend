e8abe3aa5ece4cc099bb51044d1f66d96a8d101c
infinite-persistence <inf.persistence@gmail.com>
2023-06-13 13:44:11 +0800

    Comments: fix missing "show more" for >10 replies
    ## Ticket
    2799
    
    ## Change
    Re-implemented 1f60d. To display first level of replies immediately, just need to set `showRepliesOnMount` appropriately.
    
    - No need for the extra fetch effect that caused the `page`
    variable to be out of sync with the rest.
    
    - Removed the additional logic to `hasMore`. `hasMore` is for vertical expansion while `threadLevel` is horizontal expansion, so they are not related to each other. Asked Rave if this covers a special case, but can't recall.

--- ui/component/comment/view.jsx
+++ ui/component/comment/view.jsx
@@ -181,7 +181,7 @@ function CommentView(props: Props & StateProps & DispatchProps) {
     linkedCommentId &&
     linkedCommentAncestors[linkedCommentId] &&
     linkedCommentAncestors[linkedCommentId].includes(commentId);
-  const showRepliesOnMount = isThreadComment || isInLinkedCommentChain;
+  const showRepliesOnMount = isThreadComment || isInLinkedCommentChain || (threadLevel === 0 && !!comment.replies);
 
   const [isReplying, setReplying] = React.useState(false);
   const [isEditing, setEditing] = useState(false);
@@ -199,14 +199,6 @@ function CommentView(props: Props & StateProps & DispatchProps) {
   const slimedToDeath =
     !commentByOwnerOfContent && totalLikesAndDislikes >= 5 && dislikesCount / totalLikesAndDislikes > 0.8;
   const stickerFromMessage = parseSticker(message);
-
-  React.useEffect(() => {
-    if (threadLevel === 0 && comment.replies && uri) {
-      fetchReplies(uri, commentId, page, COMMENT_PAGE_SIZE_REPLIES, SORT_BY.OLDEST);
-      setShowReplies(true);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- on mount only?
-  }, []);
 
   const isSprout = channelAge && Math.round((new Date() - channelAge) / (1000 * 60 * 60 * 24)) < 7;
 
@@ -556,7 +548,7 @@ function CommentView(props: Props & StateProps & DispatchProps) {
               threadCommentId={threadCommentId}
               numDirectReplies={numDirectReplies}
               onShowMore={handleShowMore}
-              hasMore={page < totalReplyPages && threadLevel > 0}
+              hasMore={page < totalReplyPages}
               threadDepthLevel={threadDepthLevel}
             />
           ))}
