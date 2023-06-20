9eb13c284ebc5486909be035fe2fd513051ab2d0
infinite-persistence <inf.persistence@gmail.com>
2023-06-20 12:43:21 +0800

    PdfViewer: convert to functional component

--- ui/component/viewers/pdfViewer.jsx
+++ ui/component/viewers/pdfViewer.jsx
@@ -2,22 +2,21 @@
 import * as React from 'react';
 import IframeReact from 'component/IframeReact';
 
-type Props = {
+type Props = {|
   source: string,
-};
+|};
 
-class PdfViewer extends React.PureComponent<Props> {
-  render() {
-    const { source } = this.props;
-    const src = IS_WEB ? source : `file://${source}`;
-    return (
-      <div className="file-viewer file-viewer--document">
-        <div className="file-viewer file-viewer--iframe">
-          <IframeReact title={__('File preview')} src={src} />
-        </div>
+function PdfViewer(props: Props) {
+  const { source } = props;
+  const src = IS_WEB ? source : `file://${source}`;
+
+  return (
+    <div className="file-viewer file-viewer--document">
+      <div className="file-viewer file-viewer--iframe">
+        <IframeReact title={__('File preview')} src={src} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default PdfViewer;
