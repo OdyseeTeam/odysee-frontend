27d432215380f68615c2543df021f3c40b8cb1ea
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2023-07-17 10:26:03 +0200

    Merge 1

--- ui/page/discover/index.js
+++ ui/page/discover/index.js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux';
 import { doResolveUri } from 'redux/actions/claims';
 import { selectClaimForUri } from 'redux/selectors/claims';
 import * as SETTINGS from 'constants/settings';
-import { selectActiveLivestreams } from 'redux/selectors/livestream';
-import { selectClientSetting, selectLanguage } from 'redux/selectors/settings';
-// import { selectUserHasOdyseePremiumPlus } from 'redux/selectors/memberships';
+import { selectClientSetting } from 'redux/selectors/settings';
+import { selectUserHasOdyseePremiumPlus } from 'redux/selectors/memberships';
 import DiscoverPage from './view';
 
 const select = (state, props) => {
@@ -17,10 +16,7 @@ const select = (state, props) => {
     repostedUri: repostedUri,
     repostedClaim: repostedUri ? selectClaimForUri(state, repostedUri, false) : null,
     tileLayout: selectClientSetting(state, SETTINGS.TILE_LAYOUT),
-    activeLivestreams: selectActiveLivestreams(state),
-    languageSetting: selectLanguage(state),
-    searchInLanguage: selectClientSetting(state, SETTINGS.SEARCH_IN_LANGUAGE),
-    // hasPremiumPlus: selectUserHasOdyseePremiumPlus(state),
+    hasPremiumPlus: selectUserHasOdyseePremiumPlus(state),
   };
 };
 
