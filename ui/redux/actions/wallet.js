e4d1f29f23e2b2665dc9310efe71df424fdb9113
miko <sauce47@posteo.net>
2023-08-18 09:46:31 +0300

    Typo

--- ui/redux/actions/wallet.js
+++ ui/redux/actions/wallet.js
@@ -258,7 +258,7 @@ export function doTipClaimMass() {
   };
 }
 
-export function doSpentEverything() {
+export function doSpendEverything() {
   return async (dispatch) => {
     dispatch({
       type: ACTIONS.SPENT_EVERYTHING_STARTED,
