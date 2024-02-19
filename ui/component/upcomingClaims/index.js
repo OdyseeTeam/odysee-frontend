892c9a6eb353914bd9bb6a0414781d584f734761
miko <sauce47@posteo.net>
2024-02-19 19:01:41 +0200

    Add some buffer for scheduled stream search release_time

--- ui/component/upcomingClaims/index.js
+++ ui/component/upcomingClaims/index.js
@@ -37,10 +37,7 @@ const selectOptions = createSelector(
       not_channel_ids: mutedAndBlockedIds,
       not_tags: CsOptHelper.not_tags(),
       order_by: ['^release_time'],
-      release_time: [
-        `>${moment().subtract(LIVESTREAM_UPCOMING_BUFFER, 'minutes').startOf('minute').unix()}`,
-        `>${Math.floor(moment().startOf('minute').unix())}`,
-      ],
+      release_time: [`>${moment().subtract(LIVESTREAM_UPCOMING_BUFFER, 'minutes').startOf('minute').unix()}`],
       ...(isLivestream ? { has_no_source: true } : { has_source: true }),
       ...(isLivestream && limitPerChannel ? { limit_claims_per_channel: limitPerChannel } : {}),
     };
