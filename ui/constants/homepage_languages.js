52d900ff91bec35616f7a851a51c1a4dc6d951d7
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2024-05-31 15:45:08 -0400

    India test (#3106)
    * add india homepage
    
    * Add Indian homepage
    
    * fix space
    
    * in > hi

--- ui/constants/homepage_languages.js
+++ ui/constants/homepage_languages.js
@@ -9,14 +9,21 @@ const HOMEPAGE_LANGUAGES = {
   ru: getLanguageEngName('ru'),
   it: getLanguageEngName('it'),
   'pt-BR': getLanguageEngName('pt-BR'),
+  hi: getLanguageEngName('hi'),
 };
 
 export function getHomepageLanguage(code) {
-  // -----override-----
-  if (code === 'zh-Hans' || code === 'zh-Hant') return HOMEPAGE_LANGUAGES.zh;
-  // ------------------
-
-  return HOMEPAGE_LANGUAGES[code] || null;
+  switch (code) {
+    case 'zh-Hans':
+    case 'zh-Hant':
+      return HOMEPAGE_LANGUAGES.zh;
+    case 'hi':
+    case 'pa':
+    case 'ur':
+      return HOMEPAGE_LANGUAGES.hi;
+    default:
+      return HOMEPAGE_LANGUAGES[code] || null;
+  }
 }
 
 export default HOMEPAGE_LANGUAGES;
