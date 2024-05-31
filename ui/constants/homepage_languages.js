7625e6337b86b9e0151c129b9d31f0a2517cf494
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2024-05-31 14:53:09 -0400

    Add Indian homepage

--- ui/constants/homepage_languages.js
+++ ui/constants/homepage_languages.js
@@ -13,11 +13,17 @@ const HOMEPAGE_LANGUAGES = {
 };
 
 export function getHomepageLanguage(code) {
-  // -----override-----
-  if (code === 'zh-Hans' || code === 'zh-Hant') return HOMEPAGE_LANGUAGES.zh;
-  // ------------------
-
-  return HOMEPAGE_LANGUAGES[code] || null;
+  switch (code) {
+    case 'zh-Hans':
+    case 'zh-Hant':
+      return HOMEPAGE_LANGUAGES.zh;
+    case 'hi':
+    case 'pa':
+    case 'ur':
+      return HOMEPAGE_LANGUAGES.in; // Assuming 'hi' for these languages
+    default:
+      return HOMEPAGE_LANGUAGES[code] || null;
+  }
 }
 
 export default HOMEPAGE_LANGUAGES;
