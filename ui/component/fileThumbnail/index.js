653f12ab506913c2dc7548c4b5ab3b57b8ef7b02
miko <sauce47@posteo.net>
2024-07-30 13:26:03 +0300

    Thumbnail blur

--- ui/component/fileThumbnail/index.js
+++ ui/component/fileThumbnail/index.js
@@ -1,15 +1,30 @@
 import { connect } from 'react-redux';
 import { doResolveUri } from 'redux/actions/claims';
-import { selectHasResolvedClaimForUri, selectThumbnailForUri } from 'redux/selectors/claims';
+import {
+  selectHasResolvedClaimForUri,
+  selectThumbnailForUri,
+  makeSelectTagInClaimOrChannelForUri,
+  selectIsNsfwAknowledgedForClaimId,
+  selectClaimForUri,
+  selectClaimIsMine,
+} from 'redux/selectors/claims';
 import CardMedia from './view';
+
+import { NSFW_CONTENT_TAG } from 'constants/tags';
 
 const select = (state, props) => {
   const { uri, secondaryUri } = props;
+
+  const claim = selectClaimForUri(state, uri);
+  const { claim_id: claimId } = claim || {};
 
   return {
     hasResolvedClaim: uri ? selectHasResolvedClaimForUri(state, uri) : undefined,
     thumbnailFromClaim: selectThumbnailForUri(state, uri),
     thumbnailFromSecondaryClaim: selectThumbnailForUri(state, secondaryUri, true),
+    isNsfw: makeSelectTagInClaimOrChannelForUri(props.uri, NSFW_CONTENT_TAG)(state),
+    isNsfwAknowledged: selectIsNsfwAknowledgedForClaimId(state, claimId),
+    claimIsMine: Boolean(selectClaimIsMine(state, claim)),
   };
 };
 
