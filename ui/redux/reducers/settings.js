de3874153398dab0dc2f4be5d44794602139250b
miko <sauce47@posteo.net>
2024-09-24 19:07:20 +0300

    Default theme from 'dark' to `system`

--- ui/redux/reducers/settings.js
+++ ui/redux/reducers/settings.js
@@ -35,7 +35,7 @@ const defaultState = {
     // UI
     [SETTINGS.LANGUAGE]: null,
     [SETTINGS.SEARCH_IN_LANGUAGE]: false,
-    [SETTINGS.THEME]: __('dark'),
+    [SETTINGS.THEME]: __('system'),
     [SETTINGS.THEMES]: [__('dark'), __('light'), __('system')],
     [SETTINGS.HOMEPAGE]: null,
     [SETTINGS.HOMEPAGE_ORDER]: { active: null, hidden: null },
