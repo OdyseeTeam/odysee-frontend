64b430014a8ed59d916e98965cfb32a2f400475b
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2024-10-31 16:21:49 -0400

    add ability to hide support button

--- ui/component/claimSupportButton/index.js
+++ ui/component/claimSupportButton/index.js
@@ -1,21 +1,28 @@
 import { connect } from 'react-redux';
 import { doOpenModal } from 'redux/actions/app';
 import { makeSelectTagInClaimOrChannelForUri, selectClaimForUri } from 'redux/selectors/claims';
+import { getChannelIdFromClaim } from 'util/claim';
 import ClaimSupportButton from './view';
 import { selectPreferredCurrency } from 'redux/selectors/settings';
 import { selectCanReceiveFiatTipsForUri } from 'redux/selectors/stripe';
 import { doTipAccountCheckForUri } from 'redux/actions/stripe';
 
 const DISABLE_SUPPORT_TAG = 'disable-support';
+const DISABLED_SUPPORT = process.env.DISABLED_SUPPORT_CHANNELS ? process.env.DISABLED_SUPPORT_CHANNELS.split(',') : [];
 
 const select = (state, props) => {
   const { uri } = props;
 
   const claim = selectClaimForUri(state, uri);
   const isRepost = claim && claim.repost_url;
+  const channelClaimId = claim && getChannelIdFromClaim(claim);
+
+  const disableSupport =
+    makeSelectTagInClaimOrChannelForUri(uri, DISABLE_SUPPORT_TAG)(state) ||
+    (channelClaimId && DISABLED_SUPPORT.includes(channelClaimId));
 
   return {
-    disableSupport: makeSelectTagInClaimOrChannelForUri(uri, DISABLE_SUPPORT_TAG)(state),
+    disableSupport,
     isRepost,
     preferredCurrency: selectPreferredCurrency(state),
     canReceiveFiatTips: selectCanReceiveFiatTipsForUri(state, uri),
