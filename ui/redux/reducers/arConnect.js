6d1d331ee9fbf5d31203ed8869f41d1652e3f494
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-01-30 14:05:37 +0100

    Fix more errors

--- ui/redux/reducers/arConnect.js
+++ ui/redux/reducers/arConnect.js
@@ -3,11 +3,6 @@ import * as ACTIONS from 'constants/action_types';
 // import { handleActions } from 'util/redux-utils';
 const reducers = {};
 
-type ArConnectState = {
-  status: 'loading' | 'connected' | 'disconnected',
-  address?: string,
-};
-
 const defaultState: ArConnectState = {
   status: 'loading',
   address: undefined,
--- ui/redux/selectors/arConnect.js
+++ ui/redux/selectors/arConnect.js
@@ -1,2 +1,4 @@
+// @flow
+
 const selectState = (state: State) => state.arConnect || {};
 export const selectArConnectStatus = (state: State) => selectState(state);
