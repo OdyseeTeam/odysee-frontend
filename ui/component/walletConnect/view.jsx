d0e29566108392730ebac4a578bec4811d8b1365
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-01-30 12:38:14 +0100

    Add Payment Account Page

--- ui/component/walletConnect/view.jsx
+++ ui/component/walletConnect/view.jsx
@@ -7,18 +7,8 @@ type Props = {
   connectArConnect: () => void,
 };
 
-export default function WalletConnect(props) {
-  const { wallet, setWallet, connectArConnect } = props;
-  const [walletAddress, setWalletAddress] = React.useState(null);
-
-  console.log('Props:', props);
-
-  const [walletType, setWalletType] = React.useState(null);
-  const WALLET_PERMISSIONS = ['ACCESS_ADDRESS', 'ACCESS_PUBLIC_KEY', 'SIGN_TRANSACTION', 'DISPATCH', 'SIGNATURE'];
-  const WalletEnum = {
-    arConnect: 'arConnect',
-    othent: 'othent',
-  };
+export default function WalletConnect(props: Props) {
+  const { connectArConnect } = props;
 
   React.useEffect(() => {
     console.log('dryRun');
@@ -39,32 +29,7 @@ export default function WalletConnect(props) {
 
   async function handleArConnect() {
     connectArConnect();
-    /*
-    console.log('handleArConnect');
-    if (!walletAddress) {
-      if (window.arweaveWallet) {
-        try {
-          await global.window?.arweaveWallet?.connect(WALLET_PERMISSIONS);
-          setWalletAddress(await global.window.arweaveWallet.getActiveAddress());
-          setWallet(window.arweaveWallet);
-          setWalletType(WalletEnum.arConnect);
-          // setWalletModalVisible(false);
-          localStorage.setItem('walletType', WalletEnum.arConnect);
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    }
-      */
   }
 
-  async function handleDisconnect() {
-    if (localStorage.getItem('walletType')) localStorage.removeItem('walletType');
-    await global.window?.arweaveWallet?.disconnect();
-    setWallet(null);
-    setWalletAddress(null);
-    // setProfile(null);
-  }
-
-  return <>{!wallet && <Button button="primary" onClick={handleArConnect} label="Connect" icon={ICONS.ARCONNECT} />}</>;
+  return <Button button="primary" onClick={handleArConnect} label="Connect" icon={ICONS.ARCONNECT} />;
 }
