e48e9d0ed2325fe9548b91b4b1f86160586ee725
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-02-04 15:22:50 +0100

    Fix publish form

--- ui/component/walletTipAmountSelector/view.jsx
+++ ui/component/walletTipAmountSelector/view.jsx
@@ -62,7 +62,6 @@ function WalletTipAmountSelector(props: Props) {
     doTipAccountCheckForUri,
   } = props;
 
-  console.log('activeTab: ', activeTab);
   const isMobile = useIsMobile();
   const [useCustomTip, setUseCustomTip] = usePersistedState('comment-support:useCustomTip', true);
 
@@ -203,8 +202,6 @@ function WalletTipAmountSelector(props: Props) {
 
   let fiatIconToUse = ICONS.FINANCE;
   if (preferredCurrency === 'EUR') fiatIconToUse = ICONS.EURO;
-
-  console.log('customTipAmount: ', customTipAmount);
 
   return (
     <>
