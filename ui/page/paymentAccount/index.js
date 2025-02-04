fc42a9f9a4a6c0d4f5656ff607b349f2400a3a92
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-02-04 10:21:32 +0100

    Get balance & adjust tipping modal

--- ui/page/paymentAccount/index.js
+++ ui/page/paymentAccount/index.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
-import { selectArweaveConnected } from '../../redux/selectors/arwallet';
-import { doArDisconnect } from '../../redux/actions/arwallet';
+import { selectArweaveConnected, selectArweaveBalance } from 'redux/selectors/arwallet';
+import { doArDisconnect } from 'redux/actions/arwallet';
 import PaymentAccountPage from './view';
 
 const select = (state) => ({
   arWalletStatus: selectArweaveConnected(state),
+  balance: selectArweaveBalance(state),
 });
 
 export default connect(select, {
--- ui/redux/selectors/arwallet.js
+++ ui/redux/selectors/arwallet.js
@@ -6,3 +6,4 @@ export const selectArNagged = (state: State) => selectState(state).nagged;
 export const selectArweaveConnected = (state: State) => Boolean(selectState(state).address);
 export const selectArweaveConnecting = (state: State) => selectState(state).connecting;
 export const selectArweaveAddress = (state: State) => selectState(state).address;
+export const selectArweaveBalance = (state: State) => selectState(state).balance;
