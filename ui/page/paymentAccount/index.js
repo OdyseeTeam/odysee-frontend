6a6c53a9973b65a70a398f356442a8b64b0472be
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-02-11 14:17:29 +0100

    Rewrite payment account

--- ui/page/paymentAccount/index.js
+++ ui/page/paymentAccount/index.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import { selectArweaveConnected, selectArweaveBalance } from 'redux/selectors/arwallet';
 import { doArDisconnect } from 'redux/actions/arwallet';
+import { selectThemePath } from 'redux/selectors/settings';
 import PaymentAccountPage from './view';
 
 const select = (state) => ({
   arWalletStatus: selectArweaveConnected(state),
   balance: selectArweaveBalance(state).usdc,
+  theme: selectThemePath(state),
 });
 
 export default connect(select, {
