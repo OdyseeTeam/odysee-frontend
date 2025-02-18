ad8c4741365f69da373af2add0652677709c7d2d
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-02-18 15:02:52 +0100

    Clean code

--- ui/component/walletConnect/index.js
+++ ui/component/walletConnect/index.js
@@ -1,12 +1,11 @@
 import { connect } from 'react-redux';
 import { doArConnect } from 'redux/actions/arwallet';
-import { selectArweaveStatus, selectArweaveAddress, selectArweaveConnecting } from 'redux/selectors/arwallet';
+import { selectArweaveAddress, selectArweaveConnecting } from 'redux/selectors/arwallet';
 import WalletConnect from './view';
 
 const select = (state) => ({
   arweaveAddress: selectArweaveAddress(state),
   connecting: selectArweaveConnecting(state),
-  test: selectArweaveStatus(state),
 });
 
 const perform = (dispatch) => ({
