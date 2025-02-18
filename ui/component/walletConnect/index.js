d7a2705448df41e014ef5cdeb89906e0340747f7
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-02-18 15:01:51 +0100

    Fix auto-reconnect

--- ui/component/walletConnect/index.js
+++ ui/component/walletConnect/index.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import { doArConnect } from 'redux/actions/arwallet';
-import { selectArweaveAddress } from 'redux/selectors/arwallet';
+import { selectArweaveStatus, selectArweaveAddress, selectArweaveConnecting } from 'redux/selectors/arwallet';
 import WalletConnect from './view';
 
 const select = (state) => ({
   arweaveAddress: selectArweaveAddress(state),
+  connecting: selectArweaveConnecting(state),
+  test: selectArweaveStatus(state),
 });
 
 const perform = (dispatch) => ({
