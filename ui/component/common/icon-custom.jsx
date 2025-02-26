97f21e97c08e4e1cd5bdd514d2095e64dc1e0212
miko <sauce47@posteo.net>
2025-02-26 17:38:07 +0200

    Make follow button icon more intuitive on mobile

--- ui/component/common/icon-custom.jsx
+++ ui/component/common/icon-custom.jsx
@@ -348,14 +348,13 @@ export const icons = {
     </g>
   ),
   [ICONS.SUBSCRIBE]: (props: IconProps) => {
-    const { size = 24, color = 'currentColor', ...otherProps } = props;
+    const { size = 24, ...otherProps } = props;
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         width={size}
         height={size}
-        fill={color}
         stroke={'#FFFFFF'}
         strokeWidth="2"
         strokeLinecap="round"
