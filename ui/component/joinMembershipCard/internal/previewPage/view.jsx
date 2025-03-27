f32c0e3c37c8ef39ac675910eb11b726d8b627de
zeppi <jessopb@gmail.com>
2025-03-27 10:57:21 -0400

    filter disabled from memberships tab

--- ui/component/joinMembershipCard/internal/previewPage/view.jsx
+++ ui/component/joinMembershipCard/internal/previewPage/view.jsx
@@ -140,7 +140,7 @@ const PreviewPage = (props: Props) => {
         <div className={'Error'}>{__('Payment Services are temporarily disabled. Please check back later.')}</div>
 
         <div className="join-membership__tab">
-          {creatorMemberships.map((membership, index) => (
+          {creatorMemberships.filter(m => m.enabled === true).map((membership, index) => (
             <MembershipTier
               membership={membership}
               handleSelect={() => {
