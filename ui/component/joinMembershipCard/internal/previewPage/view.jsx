939d59165bc9c2057b9f73668d7628c0301f2676
zeppi <jessopb@gmail.com>
2025-03-27 10:57:21 -0400

    filter disabled from memberships tab

--- ui/component/joinMembershipCard/internal/previewPage/view.jsx
+++ ui/component/joinMembershipCard/internal/previewPage/view.jsx
@@ -133,7 +133,7 @@ const PreviewPage = (props: Props) => {
         )}
 
         <div className="join-membership__tab">
-          {creatorMemberships.map((membership, index) => (
+          {creatorMemberships.filter(m => m.enabled === true).map((membership, index) => (
             <MembershipTier
               membership={membership}
               handleSelect={() => {
