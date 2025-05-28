b7f5b9ad19e6024caf1ca01bdc7657da978af687
zeppi <jessopb@gmail.com>
2025-05-28 16:53:11 -0400

    stop pending memberships from crashing page

--- ui/page/creatorMemberships/supporterArea/pledgesTab/internal/membershipRow/view.jsx
+++ ui/page/creatorMemberships/supporterArea/pledgesTab/internal/membershipRow/view.jsx
@@ -25,7 +25,7 @@ export default function MembershipRow(props: Props) {
   const memberChannelUri = memberChannelName === ''
     ? ''
     : buildURI({ channelName: memberChannelName, channelClaimId: activeChannelClaim.claim_id });
-  if (!creatorChannelClaim) {
+  if (!creatorChannelClaim || !membershipSub) {
     return (
       <tr>
         <td colSpan="7">
@@ -41,9 +41,10 @@ export default function MembershipRow(props: Props) {
   });
   const creatorChannelPath = formatLbryUrlForWeb(creatorChannelUri);
 
-  const currency = membershipSub.subscription.current_price.currency.toUpperCase();
-  const supportAmount = membershipSub.subscription.current_price.amount; // in cents or 1/100th EUR
-  const interval = membershipSub.subscription.current_price.frequency;
+  const currency = membershipSub.subscription.current_price
+    ? membershipSub.subscription.current_price.currency.toUpperCase() : '';
+  const supportAmount = membershipSub.subscription.current_price ? membershipSub.subscription.current_price.amount : ''; // in cents or 1/100th EUR
+  const interval = membershipSub.subscription.current_price ? membershipSub.subscription.current_price.frequency : '';
 
   const startDate = new Date(membershipSub.subscription.started_at);
   const endDate = membershipSub.subscription.ends_at === '0001-01-01T00:00:00Z' ? new Date(Date.now()).toISOString() : new Date(membershipSub.subscription.ends_at);
@@ -71,7 +72,7 @@ export default function MembershipRow(props: Props) {
       <td>{timeAgoInMonths}</td>
 
       <td>
-        ${supportAmount / 100} {currency} / {__(toCapitalCase(interval))}
+        {supportAmount ? `${supportAmount / 100} ${currency} / ${__(toCapitalCase(interval))}` : null}
       </td>
       <td>
         {membershipSub.subscription.status === 'active'
