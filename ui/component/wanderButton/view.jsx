0e68ee50f2414575c911fd2212b8068903082e6e
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-06-12 15:53:20 +0200

    Add wallet button to header

---
+++ ui/component/wanderButton/view.jsx
@@ -0,0 +1,61 @@
+// @flow
+import React from 'react';
+// $FlowIgnore
+import { useArStatus } from 'effects/use-ar-status';
+import Tooltip from 'component/common/tooltip';
+import { useHistory } from 'react-router-dom'
+import * as PAGES from 'constants/pages';
+import './style.scss';
+
+type Props = {
+  arweaveAddress: string,
+  arweaveStatus: any
+};
+
+export default function WanderButton(props: Props) {
+  const { arweaveStatus } = props;
+  const {
+    activeArStatus,
+  } = useArStatus();  
+  const history = useHistory()
+
+
+  console.log('activeArStatus: ', activeArStatus)
+  console.log('arweaveStatus: ', arweaveStatus)
+  const handleWalletClick= () => {
+    history.push(`/$/${PAGES.WALLET}`)
+  }
+
+return (
+<Tooltip
+  title={
+    activeArStatus === 'authenticating'
+      ? __('Authenticating...')
+      : activeArStatus === 'connected'
+      ? arweaveStatus.balance.ar > 0
+        ? __('Immediately spendable: $%spendable_balance_usd% (%spendable_balance_ar% AR)', { spendable_balance_usd: (arweaveStatus.balance.ar*arweaveStatus.exchangeRates.ar).toFixed(2), spendable_balance_ar: arweaveStatus.balance.ar.toFixed(6) })
+        : __('Your Wallet')
+      : ''
+  }
+>
+
+    <div
+      className={`wanderButton${
+        activeArStatus === 'authenticating'
+          ? ' wanderButton--authenticating'
+          : activeArStatus === 'connecting'
+          ? ' wanderButton--connecting'
+          : activeArStatus === 'connected'
+          ? ' wanderButton--connected'
+          : ''
+      }`}
+      onClick={handleWalletClick}
+    >
+      ${activeArStatus === 'connected' ? `${(arweaveStatus.balance.ar*arweaveStatus.exchangeRates.ar).toFixed(2)}` : ''}
+    </div>
+  </Tooltip>
+);
+
+
+
+}
