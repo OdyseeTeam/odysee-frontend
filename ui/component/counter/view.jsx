9aaf6a80f0ca8c587a0f40867526ab5f2767ffb3
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-06-13 13:29:44 +0200

    Improve counter animation

--- ui/component/counter/view.jsx
+++ ui/component/counter/view.jsx
@@ -5,17 +5,25 @@ import './style.scss'
 const DIGITS = Array.from({ length: 10 }, (_, i) => i)
 
 export default function Counter({ value }) {
-  const [chars, setChars] = React.useState(['0','.','0','0'])
+  const [chars, setChars] = React.useState([])
+  const [initialized, setInitialized] = React.useState(false)
 
   React.useEffect(() => {
+    const str = Number(value).toFixed(2).split('')
+
+    if (!initialized) {
+      setChars(str)
+      setInitialized(true)
+      return
+    }
+
     const timeout = setTimeout(() => {
-      const str = Number(value).toFixed(2)
-      setChars(str.split(''))
+      setChars(str)
     }, 200)
 
     return () => clearTimeout(timeout)
   }, [value])
-
+  
   return (
     <div className="counter-wrapper" style={{ lineHeight: 1 }}>
       {chars.map((c, i) =>
