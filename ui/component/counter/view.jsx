82b384d250283aa25a5b78cd3d2aa35439231f31
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-06-13 13:23:42 +0200

    Improve counter animation

--- ui/component/counter/view.jsx
+++ ui/component/counter/view.jsx
@@ -2,25 +2,25 @@
 import React from 'react'
 import './style.scss'
 
-
 const DIGITS = Array.from({ length: 10 }, (_, i) => i)
 
 export default function Counter({ value }) {
-  const [chars, setChars] = React.useState([])
+  const [chars, setChars] = React.useState(['0','.','0','0'])
 
   React.useEffect(() => {
-    const str = Number(value).toFixed(2)
-    setChars(str.split(''))
+    const timeout = setTimeout(() => {
+      const str = Number(value).toFixed(2)
+      setChars(str.split(''))
+    }, 200)
+
+    return () => clearTimeout(timeout)
   }, [value])
 
   return (
     <div className="counter-wrapper" style={{ lineHeight: 1 }}>
       {chars.map((c, i) =>
         /\d/.test(c) ? (
-          <div
-            className="number-wrapper"
-            key={i}
-          >
+          <div className="number-wrapper" key={i}>
             <div
               className="digit-stack"
               style={{
@@ -28,14 +28,14 @@ export default function Counter({ value }) {
               }}
             >
               {DIGITS.map(d => (
-                <div className="digit" key={d} >
+                <div className="digit" key={d}>
                   {d}
                 </div>
               ))}
             </div>
           </div>
         ) : (
-          <span key={i} className="number-placeholder" >
+          <span key={i} className="number-placeholder">
             {c}
           </span>
         )
