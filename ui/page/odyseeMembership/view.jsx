74b61a3944518beb0e5c6a8578c3820f19364b10
zeppi <jessopb@gmail.com>
2025-06-24 13:36:01 -0400

    update odysee premium page

--- ui/page/odyseeMembership/view.jsx
+++ ui/page/odyseeMembership/view.jsx
@@ -1,146 +1,32 @@
 // @flow
 import React from 'react';
-import { ODYSEE_CHANNEL } from 'constants/channels';
-import * as ICONS from 'constants/icons';
-import usePersistedState from 'effects/use-persisted-state';
 import Page from 'component/page';
-import Spinner from 'component/spinner';
 import Card from 'component/common/card';
-import MembershipSplash from 'component/membershipSplash';
-import Button from 'component/button';
 import ChannelSelector from 'component/channelSelector';
-import HelpText from './internal/help-text';
-import ClearMembershipDataButton from 'component/clearMembershipData';
-import PremiumOption from './internal/premiumOption';
 import './style.scss';
 
 type Props = {
   // -- redux --
-  mineFetched: boolean,
-  mineFetching: boolean,
-  validMemberships: ?MembershipSubs,
-  activeMemberships: ?MembershipSubs,
-  purchasedMemberships: ?MembershipSubs,
-  canceledMemberships: ?MembershipSubs,
-  membershipOptions: ?CreatorMemberships,
-  doMembershipList: (params: MembershipListParams) => Promise<CreatorMemberships>,
+  hasOdyseeLegacy: boolean,
 };
 
 const OdyseeMembershipPage = (props: Props) => {
   const {
     // -- redux --
-    mineFetched,
-    mineFetching,
-    validMemberships,
-    activeMemberships,
-    purchasedMemberships,
-    canceledMemberships,
-    membershipOptions,
-    doMembershipList,
+    hasOdyseeLegacy,
   } = props;
-
-  const [hasShownModal, setHasShownModal] = React.useState(false);
-  const [showHelp, setShowHelp] = usePersistedState('premium-help-seen', true);
-
-  React.useEffect(() => {
-    doMembershipList({ channel_claim_id: ODYSEE_CHANNEL.ID });
-  }, [doMembershipList]);
-
-  // we are still waiting from the backend if any of these are undefined
-  const stillWaitingFromBackend = (membershipOptions === undefined || !mineFetched) && mineFetching;
-
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const interval = urlSearchParams.get('interval') || undefined;
-  const planValue = urlSearchParams.get('plan') || undefined;
-
-  // add a bit of a delay otherwise it's a bit jarring
-  const timeoutValue = 300;
-
-  // if user already selected plan, wait a bit (so it's not jarring) and open modal
-  React.useEffect(() => {
-    if (!stillWaitingFromBackend && planValue) {
-      const delayTimeout = setTimeout(() => {
-        // clear query params
-        window.history.replaceState(null, null, window.location.pathname);
-
-        setHasShownModal(true);
-
-        // open confirm purchase
-        // $FlowFixMe
-        document.querySelector('[plan="' + planValue + '"][interval="' + interval + '"]').click();
-      }, timeoutValue);
-
-      return () => clearTimeout(delayTimeout);
-    }
-  }, [stillWaitingFromBackend, planValue, interval]);
-
-  if (stillWaitingFromBackend) {
-    return (
-      <Page className="premium-wrapper">
-        <div className="card-stack">
-          <div className="main--empty">
-            <Spinner />
-          </div>
-        </div>
-      </Page>
-    );
-  } else if (!mineFetched) {
-    return (
-      <Page className="premium-wrapper">
-        <div className="card-stack">
-          <div className="main--empty">
-            <p>{__('Failed to fetch memberships')}</p>
-          </div>
-        </div>
-      </Page>
-    );
-  }
-
-  if (purchasedMemberships?.length === 0 && !planValue && !hasShownModal) {
-    return (
-      <Page className="premium-wrapper">
-        <MembershipSplash pageLocation="confirmPage" />
-      </Page>
-    );
-  }
 
   return (
     <Page className="premium-wrapper card-stack">
       <Card title={__('Odysee Premium')} subtitle={<ChannelSelector />}>
-        <div className="card-stack">
           <Card
             className="premium-explanation-text"
-            title={__('Get More Information')}
-            titleActions={
-              <Button button="close" icon={showHelp ? ICONS.UP : ICONS.DOWN} onClick={() => setShowHelp(!showHelp)} />
-            }
-            subtitle={__('Expand to learn more about how Odysee Premium works')}
-            body={showHelp && <HelpText />}
+            title={__('Legacy Odysee Premium Membership')}
+            subtitle={hasOdyseeLegacy
+              ? __('Thank you for being a Legacy Odysee Premium Member. You will continue to have these benefits going forward as a token of our appreciation.')
+              : __('Purchasing Odysee Premium Membership is not available at this time.')}
           />
-
-          {activeMemberships && activeMemberships.length > 0 && (
-            <Card title={__('Your Active Memberships')}>
-              {activeMemberships.map((membership) => (
-                <PremiumOption key={membership.name} membershipView={membership} />
-              ))}
-            </Card>
-          )}
-
-          {canceledMemberships && (
-            <Card title={__('Canceled Memberships')}>
-              {canceledMemberships.length === 0 ? (
-                <h4>{__('You currently have no canceled memberships')}</h4>
-              ) : (
-                canceledMemberships.map((membership) => (
-                  <PremiumOption key={membership.name} membershipView={membership} />
-                ))
-              )}
-            </Card>
-          )}
-        </div>
       </Card>
-
-      <ClearMembershipDataButton purchasedMemberships={purchasedMemberships} />
     </Page>
   );
 };
