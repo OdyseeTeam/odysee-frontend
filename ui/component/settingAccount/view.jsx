a63bd2d2354b3ee372d7fb47cfa57b70b5393187
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-07-03 17:20:05 +0200

    Remove stripe options in settings

--- ui/component/settingAccount/view.jsx
+++ ui/component/settingAccount/view.jsx
@@ -8,7 +8,6 @@ import Card from 'component/common/card';
 import SettingsRow from 'component/settingsRow';
 import SyncToggle from 'component/syncToggle';
 import { getPasswordFromCookie } from 'util/saved-passwords';
-import { getStripeEnvironment } from 'util/stripe';
 
 type Props = {
   // --- redux ---
@@ -57,38 +56,6 @@ export default function SettingAccount(props: Props) {
 
             {/* @if TARGET='app' */}
             <SyncToggle disabled={walletEncrypted && !storedPassword && storedPassword !== ''} />
-            {/* @endif */}
-
-            {/* @if TARGET='web' */}
-            {user && getStripeEnvironment() && (
-              <SettingsRow
-                title={__('Bank Accounts')}
-                subtitle={__('Connect a bank account to receive tips and compensation in your local currency.')}
-              >
-                <Button
-                  button="inverse"
-                  label={__('Manage')}
-                  icon={ICONS.ARROW_RIGHT}
-                  navigate={`/$/${PAGES.SETTINGS_STRIPE_ACCOUNT}`}
-                />
-              </SettingsRow>
-            )}
-            {/* @endif */}
-
-            {/* @if TARGET='web' */}
-            {isAuthenticated && getStripeEnvironment() && (
-              <SettingsRow
-                title={__('Payment Methods')}
-                subtitle={__('Add a credit card to tip creators in their local currency.')}
-              >
-                <Button
-                  button="inverse"
-                  label={__('Manage')}
-                  icon={ICONS.ARROW_RIGHT}
-                  navigate={`/$/${PAGES.SETTINGS_STRIPE_CARD}`}
-                />
-              </SettingsRow>
-            )}
             {/* @endif */}
 
             {hasChannels && (
