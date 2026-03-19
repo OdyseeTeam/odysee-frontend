bf37c8760509b56090129feb4f543b4c9b5a2e53
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2026-03-19 16:08:46 +0100

    iPhone patch

--- ui/util/full-screen.js
+++ ui/util/full-screen.js
@@ -3,6 +3,11 @@
   https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
 */
 
+import platform from 'ui/util/platform';
+
+const IOS_FS_CLASS = 'ios-fullscreen';
+let iosFsElement = null;
+
 const prefixes = {
   exitFullscreen: ['exitFullscreen', 'msExitFullscreen', 'mozCancelFullScreen', 'webkitExitFullscreen'],
   fullscreenChange: ['fullscreenchange', 'MSFullscreenChange', 'mozfullscreenchange', 'webkitfullscreenchange'],
@@ -13,36 +18,54 @@ const prefixes = {
 
 const getPrefix = () => {
   let prefixIndex = 0;
-  // validate prefix
   prefixes.fullscreenEnabled.some((prefix, index) => {
     if (document[prefix] || document[prefix] === false) {
       prefixIndex = index;
       return true;
     }
   });
-  // prefix vendor index
   return prefixIndex;
 };
 
 export const fullscreenElement = () => {
+  if (platform.isIPhone()) {
+    return iosFsElement;
+  }
   const index = getPrefix();
   const prefix = prefixes.fullscreenElement[index];
   return document[prefix];
 };
 
 export const requestFullscreen = (elem) => {
+  if (platform.isIPhone()) {
+    elem.classList.add(IOS_FS_CLASS);
+    iosFsElement = elem;
+    document.dispatchEvent(new Event('fullscreenchange'));
+    elem.dispatchEvent(new Event('fullscreenchange'));
+    return;
+  }
   const index = getPrefix();
   const prefix = prefixes.requestFullscreen[index];
   elem[prefix] && elem[prefix]();
 };
 
 export const exitFullscreen = () => {
+  if (platform.isIPhone() && iosFsElement) {
+    iosFsElement.classList.remove(IOS_FS_CLASS);
+    iosFsElement = null;
+    document.dispatchEvent(new Event('fullscreenchange'));
+    return;
+  }
   const index = getPrefix();
   const prefix = prefixes.exitFullscreen[index];
   document[prefix] && document[prefix]();
 };
 
 export const onFullscreenChange = (target, action, callback) => {
+  if (platform.isIPhone()) {
+    target[`${action}EventListener`]('fullscreenchange', callback, false);
+    return;
+  }
   const index = getPrefix();
   const prefix = prefixes.fullscreenChange[index];
   target[`${action}EventListener`](prefix, callback, false);
